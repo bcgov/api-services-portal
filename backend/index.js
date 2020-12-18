99c1d258ccb9df4f5898718f43fb23b1bb81f66a
ikethecoder <ikethecoder@canzea.com>
2020-12-18 14:02:01 -0800

    upd creds for be

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const initialiseData = require('./initial-data');
 
 const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
 const PROJECT_NAME = 'app2';
-const adapterConfig = { mongoUri: process.env.MONGO_URL, auth: { user: process.env.MONGO_USER, pass: process.env.MONGO_PASSWORD } };
+const adapterConfig = { mongoUri: process.env.MONGO_URL, user: process.env.MONGO_USER, pass: process.env.MONGO_PASSWORD };
 
 require('dotenv').config()
 
