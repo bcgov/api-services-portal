e7b05266be88973da527f1415a72c520f41eba32
ikethecoder <ikethecoder@canzea.com>
2021-01-06 10:21:23 -0800

    add a sample-upstream for oauth2-proxy

---
+++ oauth2-proxy/README.md
@@ -0,0 +1,42 @@
+
+
+# Oauth Proxy
+```
+export COOKIE_SECRET=""
+export OIDC_CLIENT_ID=""
+export OIDC_CLIENT_SECRET=""
+export OIDC_ISSUER=""
+
+hostip=$(ifconfig en0 | awk '$1 == "inet" {print $2}')
+
+docker run -ti --rm --name proxy -p 4180:4180 \
+  quay.io/oauth2-proxy/oauth2-proxy \
+    --http-address=0.0.0.0:4180 \
+    --cookie-secret=$COOKIE_SECRET \
+    --email-domain=* \
+    --provider=keycloak \
+    --client-id=${OIDC_CLIENT_ID} \
+    --client-secret=${OIDC_CLIENT_SECRET} \
+    --scope=openid \
+    --oidc-issuer-url="${OIDC_ISSUER}" \
+    --login-url="${OIDC_ISSUER}/protocol/openid-connect/auth" \
+    --redeem-url="${OIDC_ISSUER}/protocol/openid-connect/token" \
+    --validate-url="${OIDC_ISSUER}/protocol/openid-connect/userinfo" \
+    --redirect-url="http://localhost:4180/oauth2/callback" \
+    --cookie-secure=False \
+    --cookie-refresh=60s \
+    --cookie-name=app.sid \
+    --pass-access-token \
+    --pass-user-headers \
+    --pass-host-header \
+    --skip-auth-regex=/public \
+    --upstream="http://${hostip}:9000"
+```
+
+# Sample Upstream
+
+```
+(cd sample-upstream && docker build --tag sample.local .)
+
+docker run -ti --rm -p 9000:9000 sample.local
+```
---
+++ oauth2-proxy/sample-upstream/index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const session = require('express-session')
+const app = express()
+const port = 9000
+
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+app.set('trust proxy', 1) // trust first proxy
+
+app.use(session({
+    secret: 's3cr3t',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }))
+
+const jwtcerts = "https://authz-apps-gov-bc-ca.dev.apsgw.xyz/auth/realms/aps/protocol/openid-connect/certs"
+
+const jwtCheck = jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: jwtcerts
+})
+
+app.all('', jwt({ 
+    secret: jwtCheck, 
+    algorithms: ['RS256'], 
+    credentialsRequired: false, 
+    requestProperty: 'user', 
+    getToken: (req) => ('x-forwarded-access-token' in req.headers) ? req.headers['x-forwarded-access-token'] : null
+}))
+
+app.all('', (req, res, next) => {
+    if ('user' in req.session && 'user' in req && req.session['user']['sub'] != req['user']['sid']) {
+        console.log("Detected different user!  Invalid session")
+        req.session.destroy()
+    }
+
+    if (!('user' in req.session) && 'user' in req) {
+        console.log(JSON.stringify(req.user, null, 4))
+        console.log("New login, set session")
+        req.session.user = req.user
+    }
+    
+    next()
+});
+
+app.get('/public', (req, res) => {
+    if ('session' in req && 'user' in req.session) {
+        res.send(`Unprotected content. Hello ${req.session.user.name} (with email ${req.session.user.email}, username ${req.session.user.preferred_username}) in ${req.session.user.namespace}!`)
+    } else {
+        res.send(`Unprotected content. Anonymous!`)
+    }
+})
+
+app.get('/', (req, res) => {
+    res.send(`Hello ${req.session.user.name} (with email ${req.session.user.email}, username ${req.session.user.preferred_username}) in ${req.session.user.namespace}!`)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+process.on('SIGINT', () => process.exit())
