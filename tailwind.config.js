c718ffe31cb9807c7e69e2810447ebce5bb4cb8e
Joshua Jones <joshua@general-metrics.com>
2021-01-08 09:51:59 -0800

    Update docker file to copy config files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   purge: [
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'media', // 'media' or 'class'
   theme: {
