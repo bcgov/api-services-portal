83a3728fcbdec136bd947219ca12142a70132b28
ikethecoder <ikethecoder@canzea.com>
2021-01-25 16:25:14 -0800

    fix auth err

--- src/authz/enforcement.js
+++ src/authz/enforcement.js
@@ -1,8 +1,15 @@
+const fs = require('fs')
 
+let rules = {
+    rulePath = fs.realpathSync('config.json', []),
+    ts,
+    cache
+}
 
 // Use a decision matrix to determine who is allowed to do what
 function EnforcementPoint ({ listKey, operation, itemId, authentication: { item } }) {
     console.log("*** ACCESS *** " + listKey + " [" + (itemId == null ? "":itemId) + "] " + operation + " by " + (item == null ? "ANON":item.name))
+    console.log(rules.rulePath)
     return true
 }
 
--- src/nextapp/types.ts
+++ src/nextapp/types.ts
@@ -2,4 +2,5 @@ export interface UserData {
   username: string;
   roles: string[];
   namespace: string;
+  userId: string;
 }
