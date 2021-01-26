50ea68f8860fc74c28dc56cdd4b2e9ecebc3f10f
ikethecoder <ikethecoder@canzea.com>
2021-01-25 16:29:44 -0800

    fix auth err

--- src/authz/enforcement.js
+++ src/authz/enforcement.js
@@ -1,15 +1,17 @@
 const fs = require('fs')
 
 let rules = {
-    rulePath = fs.realpathSync('config.json', []),
-    ts,
-    cache
+    rulePath: fs.realpathSync('authz/matrix.csv', []),
+    ts: fs.statSync('authz/matrix.csv').mtime,
+    cache: null
 }
 
 // Use a decision matrix to determine who is allowed to do what
 function EnforcementPoint ({ listKey, operation, itemId, authentication: { item } }) {
     console.log("*** ACCESS *** " + listKey + " [" + (itemId == null ? "":itemId) + "] " + operation + " by " + (item == null ? "ANON":item.name))
-    console.log(rules.rulePath)
+    if (fs.statSync('authz/matrix.csv').mtime != rules.ts) {
+        console.log("RELOAD RULES...")
+    }
     return true
 }
 
