cbb74bc139799590a6fc4030627147e5e7e4ae75
ikethecoder <ikethecoder@canzea.com>
2021-04-14 10:08:46 -0700

    fix revoke all

--- src/nextapp/pages/devportal/poc/resources/permissions.tsx
+++ src/nextapp/pages/devportal/poc/resources/permissions.tsx
@@ -57,7 +57,7 @@ function List({ data, state, granted, loginUserSub, grantAccess, revokeAccess })
                             <Td>{tagit(item.scopes, 'blue', granted ? revokeAccess : null)}</Td>
                             <Td>
                                 {granted ? (
-                                    <Button colorScheme="red" size="sm" onClick={() => revokeAccess(item.scopes.map(s => s.id))}>Revoke All</Button>
+                                    <Button colorScheme="red" size="sm" onClick={() => revokeAccess(item.scopes.map(s => s.ticketId))}>Revoke All</Button>
                                 ) : (
                                     <Button colorScheme="red" size="md" onClick={() => grantAccess(item)}>Grant Access</Button>
                                 )} 
