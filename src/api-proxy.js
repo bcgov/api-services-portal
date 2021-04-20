c742763244f02888854688d749bc9171b6ac7bb0
ikethecoder <ikethecoder@canzea.com>
2021-04-20 09:50:06 -0700

    disable nextapp

--- src/api-proxy.js
+++ src/api-proxy.js
@@ -13,8 +13,9 @@ class ApiProxyApp {
     const app = express();
     const apiProxy = createProxyMiddleware({ 
         target: this._gwaApiUrl, 
-        pathRewrite: { '^/api/': '/v2/' },
+        pathRewrite: { '^/vapi/': '/v2/' },
         onProxyReq: (proxyReq, req) => { 
+            proxyReq.setHeader('Accept', 'application/json')
             proxyReq.setHeader('Authorization', `Bearer ${req.header('x-forwarded-access-token')}`) },
         onError:(err, req, res, target) => {
             console.log(err)
@@ -24,7 +25,7 @@ class ApiProxyApp {
             res.end('error reaching api');
         }
     })
-    app.all(/^\/api/, apiProxy)
+    app.all(/^\/vapi/, apiProxy)
     return app;
   }
 }
