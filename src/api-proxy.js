e18ffb9261f0bf9221bc790e66cd70ca32b46d13
ikethecoder <ikethecoder@canzea.com>
2021-04-20 10:45:44 -0700

    upd proxy req

--- src/api-proxy.js
+++ src/api-proxy.js
@@ -17,6 +17,8 @@ class ApiProxyApp {
         pathRewrite: { '^/api/': '/v2/' },
         onProxyReq: (proxyReq, req) => {
             console.log(req.headers)
+            proxyReq.removeHeader("host");
+            proxyReq.removeHeader("cookie");
             proxyReq.setHeader('Accept', 'application/json')
             proxyReq.setHeader('Authorization', `Bearer ${req.header('x-forwarded-access-token')}`) },
         onError:(err, req, res, target) => {
