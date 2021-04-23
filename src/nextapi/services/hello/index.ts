a457b9fffad41541bfe3f5fd3d72701f577b59f1
ikethecoder <ikethecoder@canzea.com>
2021-04-23 14:48:37 -0700

    small refactor of nextapi

--- src/nextapi/services/hello/index.ts
+++ src/nextapi/services/hello/index.ts
@@ -1,27 +1,2 @@
-import api from '@/shared/services/api'
-import { Service, ServiceContext } from '@/services/context'
-
-import Hello from './hello'
-
-export class HelloService extends Service {
-    constructor(context: ServiceContext) {
-        super(context)
-    }
-
-    public async getHello(userName: string): Promise<Hello> {
-        const data = await api(query, { }, { ...{headers : this.context.headers}, ...{ssr: false} }).catch (err => {
-            console.log("Caught err " + JSON.stringify(err, null, 5))
-            return { error: 'Failed to call API'}
-        })
-
-        return { name : "Hello again " + userName, accessRequestIds: data.allAccessRequests}
-    }
-}
-
-
-const query = `
-    query a {
-        allAccessRequests {
-            id
-        }
-    }`
+export * from './model'
+export * from './service'
