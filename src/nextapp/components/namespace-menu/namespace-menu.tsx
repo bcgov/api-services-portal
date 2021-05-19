9f26d9fd0106f72aeaf31e4e7bc768cbc3bd48ec
ikethecoder <ikethecoder@canzea.com>
2021-05-18 22:42:51 -0700

    try and fix build issue with namespace

--- src/nextapp/components/namespace-menu/namespace-menu.tsx
+++ src/nextapp/components/namespace-menu/namespace-menu.tsx
@@ -35,7 +35,7 @@ const NamespaceMenu: React.FC<NamespaceMenuProps> = ({ user }) => {
   const managerDisclosure = useDisclosure();
   const { data, isLoading, isSuccess, isError } = useQuery<NamespaceData[]>(
     'allNamespaces',
-    async () => await restApi<NamespaceData[]>('/gw/api/namespaces')
+    () => restApi<NamespaceData[]>('/gw/api/namespaces') as Promise<NamespaceData[]>
   );
 
   const handleNamespaceChange = React.useCallback(
