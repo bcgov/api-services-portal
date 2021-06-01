720781f0651ea7c53796fc1a470bcb79033fb021
ikethecoder <ikethecoder@canzea.com>
2021-06-01 12:11:15 -0700

    upd AccessRequest perms

--- src/lists/AccessRequest.js
+++ src/lists/AccessRequest.js
@@ -1,98 +1,118 @@
-const { Text, Checkbox, Relationship } = require('@keystonejs/fields')
-const { Markdown } = require('@keystonejs/fields-markdown')
+const { Text, Checkbox, Relationship } = require('@keystonejs/fields');
+const { Markdown } = require('@keystonejs/fields-markdown');
 
-const { byTracking } = require('../components/ByTracking')
+const { byTracking } = require('../components/ByTracking');
 
-const { atTracking } = require('@keystonejs/list-plugins')
+const { atTracking } = require('@keystonejs/list-plugins');
 
-const { FieldEnforcementPoint, EnforcementPoint } = require('../authz/enforcement')
+const {
+  FieldEnforcementPoint,
+  EnforcementPoint,
+} = require('../authz/enforcement');
 
-const { Apply, Validate } = require('../services/workflow')
+const { Apply, Validate } = require('../services/workflow');
 
 module.exports = {
   fields: {
     name: {
-        type: Text,
-        isRequired: true,
+      type: Text,
+      isRequired: true,
     },
     communication: {
-        type: Markdown,
-        isRequired: false,
+      type: Markdown,
+      isRequired: false,
     },
     isApproved: {
       type: Checkbox,
       isRequired: false,
       default: false,
-      access: FieldEnforcementPoint
+      access: FieldEnforcementPoint,
     },
     isIssued: {
-        type: Checkbox,
-        isRequired: false,
-        default: false,
-        access: FieldEnforcementPoint
+      type: Checkbox,
+      isRequired: false,
+      default: false,
+      access: FieldEnforcementPoint,
     },
     isComplete: {
-        type: Checkbox,
-        isRequired: false,
-        default: false
+      type: Checkbox,
+      isRequired: false,
+      default: false,
     },
     credential: {
-        type: Text,
-        isRequired: false,
+      type: Text,
+      isRequired: false,
     },
     controls: {
-        type: Text,
-        isRequired: true,
+      type: Text,
+      isRequired: true,
     },
     additionalDetails: {
-        type: Text,
-        isMultiline: true,
-        isRequired: false,
-    },  
+      type: Text,
+      isMultiline: true,
+      isRequired: false,
+    },
     requestor: { type: Relationship, isRequired: true, ref: 'User' },
     application: { type: Relationship, isRequired: false, ref: 'Application' },
-    productEnvironment: { type: Relationship, isRequired: true, ref: 'Environment' },
-    serviceAccess: { type: Relationship, isRequired: false, ref: 'ServiceAccess' },
+    productEnvironment: {
+      type: Relationship,
+      isRequired: true,
+      ref: 'Environment',
+    },
+    serviceAccess: {
+      type: Relationship,
+      isRequired: false,
+      ref: 'ServiceAccess',
+    },
   },
   access: EnforcementPoint,
-  plugins: [
-    byTracking(),
-    atTracking()
-  ],
+  plugins: [byTracking(), atTracking()],
   hooks: {
-    validateInput: (async function ({
-        operation,
-        existingItem,
-        originalInput,
-        resolvedData,
-        context,
-        addFieldValidationError, // Field hooks only
-        addValidationError, // List hooks only
-        listKey,
-        fieldPath, // Field hooks only
+    validateInput: async function ({
+      operation,
+      existingItem,
+      originalInput,
+      resolvedData,
+      context,
+      addFieldValidationError, // Field hooks only
+      addValidationError, // List hooks only
+      listKey,
+      fieldPath, // Field hooks only
     }) {
-        await Validate(context, operation, existingItem, originalInput, resolvedData, addValidationError)
-    }),
-    beforeChange: ({
+      await Validate(
+        context.createContext({ skipAccessControl: true }),
         operation,
         existingItem,
         originalInput,
         resolvedData,
-        context,
-        listKey,
-        fieldPath, // exists only for field hooks
-      }) => {
+        addValidationError
+      );
     },
-    afterChange: (async function ({
+    beforeChange: ({
+      operation,
+      existingItem,
+      originalInput,
+      resolvedData,
+      context,
+      listKey,
+      fieldPath, // exists only for field hooks
+    }) => {},
+    afterChange: async function ({
+      operation,
+      existingItem,
+      originalInput,
+      updatedItem,
+      context,
+      listKey,
+      fieldPath, // exists only for field hooks
+    }) {
+      await Apply(
+        context.createContext({ skipAccessControl: true }),
         operation,
         existingItem,
         originalInput,
-        updatedItem,
-        context,
-        listKey,
-        fieldPath, // exists only for field hooks
-      }) {
-        await Apply(context.createContext({skipAccessControl:true}), operation, existingItem, originalInput, updatedItem)
-    })
-  }
-}
+        updatedItem
+      );
+    },
+  },
+};
