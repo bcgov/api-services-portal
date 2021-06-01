bf91bd0638ec30db156a82de649c7778d19595ac
ikethecoder <ikethecoder@canzea.com>
2021-06-01 11:52:25 -0700

    fix error with incorrect authz rule

--- src/authz/actions/filterBySelf.js
+++ src/authz/actions/filterBySelf.js
@@ -1,6 +1,6 @@
 const filterBySelf = (context, value) => {
-    const id = context['user']['id']
-    return { id : id }
-}
+  const id = context['user']['id'];
+  return { id: id };
+};
 
-module.exports = filterBySelf
+module.exports = filterBySelf;
--- src/lists/extensions/resolvers/itemQuery.ts
+++ src/lists/extensions/resolvers/itemQuery.ts
@@ -1,27 +1,41 @@
-import { EvalAccessControl } from './common'
-import { mergeWhereClause } from '@keystonejs/utils'
-import { AliasConfig } from './types'
+import { EvalAccessControl } from './common';
+import { mergeWhereClause } from '@keystonejs/utils';
+import { AliasConfig } from './types';
+import { Logger } from '../../../logger';
+import { EnforcementPoint } from '../../../authz/enforcement';
 
-import { EnforcementPoint } from '../../../authz/enforcement'
+const logger = Logger('ext.itemQuery');
 
-export const ItemQuery = (keystone: any, alias : AliasConfig) => {
-    return {
-        queries: [
-            {
-                schema: `${alias.gqlName}(where: ${alias.list}WhereInput): ${alias.list}`,
-                resolver: async (item : any, args : any, context : any, info : any, other : any) => {
-
-                    // authorize using the authenticed context
-                    const accessAnswer = await EvalAccessControl(alias, item, args, context, info)
-            
-                    // run the query with no access control
-                    const a = keystone.getListByKey(alias.list)
-                    const gqlName = a.gqlNames.listQueryName
-                    const noauthContext =  keystone.createContext({ skipAccessControl: true })
-                    return await a.itemQuery(mergeWhereClause(args,accessAnswer), noauthContext, gqlName, info)
-                },
-                access: EnforcementPoint
-            }
-        ]
-    }
-}
+export const ItemQuery = (keystone: any, alias: AliasConfig) => {
+  return {
+    queries: [
+      {
+        schema: `${alias.gqlName}(where: ${alias.list}WhereInput): ${alias.list}`,
+        resolver: async (
+          item: any,
+          args: any,
+          context: any,
+          info: any,
+          other: any
+        ) => {
+          const a = keystone.getListByKey(alias.list);
+          const gqlName = a.gqlNames.itemQueryName;
+          const vars = mergeWhereClause(args, other.access);
+          const noauthContext = keystone.createContext({
+            skipAccessControl: true,
+          });
+          logger.debug(
+            'item query %s %j %j %j %s',
+            alias.list,
+            args,
+            other,
+            vars,
+            gqlName
+          );
+          return await a.itemQuery(vars, noauthContext, gqlName, info);
+        },
+        access: EnforcementPoint,
+      },
+    ],
+  };
+};
