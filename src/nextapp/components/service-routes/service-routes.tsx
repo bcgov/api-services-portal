cd8315faf1f66975ee34565146a9f35bb309b5dc
ikethecoder <ikethecoder@canzea.com>
2021-06-15 23:52:15 -0700

    remove unwanted code

--- src/nextapp/components/service-routes/service-routes.tsx
+++ src/nextapp/components/service-routes/service-routes.tsx
@@ -15,55 +15,9 @@ interface ServiceRoutesProps {
 }
 
 const ServiceRoutes: React.FC<ServiceRoutesProps> = ({ routes }) => {
-  console.log(routes);
-  const d = {
-    data: {
-      GatewayService: {
-        id: '58',
-        name: 'apidata-geocoder-geocoder',
-        namespace: 'dss-loc',
-        tags:
-          '["ns.dss-loc.apidata","ns.dss-loc","production","serv-geocoder"]',
-        host: 'geocoder.lb.v1',
-        environment: {
-          id: '3',
-          name: 'prod',
-          active: true,
-          flow: 'public',
-          product: {
-            name: 'BC Address Geocoder',
-            organization: null,
-            organizationUnit: null,
-          },
-        },
-        plugins: [
-          { id: '25', name: 'statsd' },
-          { id: '24', name: 'response-transformer' },
-          { id: '33', name: 'acl' },
-          { id: '26', name: 'bcgov-gwa-endpoint' },
-          { id: '28', name: 'http-log' },
-          { id: '32', name: 'key-auth' },
-          { id: '30', name: 'cors' },
-          { id: '22', name: 'rate-limiting' },
-        ],
-        routes: [
-          {
-            id: '53',
-            name: 'apidata-geocoder-geocoder-geocoder-906eb-0',
-            hosts: '["geocoder.api.gov.bc.ca"]',
-            paths:
-              '["/addresses*","/sites/*","/intersections/*","/occupants/*"]',
-            methods: '["GET","OPTIONS"]',
-          },
-        ],
-        updatedAt: '2021-06-15T19:44:48.732Z',
-      },
-    },
-  };
-  const _routes = d.data.GatewayService.routes;
   return (
     <>
-      {_routes.map((route) => {
+      {routes.map((route) => {
         const _methods = JSON.parse(route['methods']);
         const methods =
           Array.isArray(_methods) && _methods.length > 0 ? _methods : ['ALL'];
