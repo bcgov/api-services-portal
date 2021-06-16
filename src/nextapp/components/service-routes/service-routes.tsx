89015bc0c886db8066eac317fd300247993fffe9
ikethecoder <ikethecoder@canzea.com>
2021-06-15 23:35:21 -0700

    upd the service route list

--- src/nextapp/components/service-routes/service-routes.tsx
+++ src/nextapp/components/service-routes/service-routes.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import { Stack, Tag, TagLabel, Box } from '@chakra-ui/react';
+import {
+  Stack,
+  Tag,
+  TagLabel,
+  Box,
+  SimpleGrid,
+  VStack,
+  StackDivider,
+} from '@chakra-ui/react';
 import { GatewayRoute } from '@/shared/types/query.types';
 
 interface ServiceRoutesProps {
@@ -7,9 +15,55 @@ interface ServiceRoutesProps {
 }
 
 const ServiceRoutes: React.FC<ServiceRoutesProps> = ({ routes }) => {
+  console.log(routes);
+  const d = {
+    data: {
+      GatewayService: {
+        id: '58',
+        name: 'apidata-geocoder-geocoder',
+        namespace: 'dss-loc',
+        tags:
+          '["ns.dss-loc.apidata","ns.dss-loc","production","serv-geocoder"]',
+        host: 'geocoder.lb.v1',
+        environment: {
+          id: '3',
+          name: 'prod',
+          active: true,
+          flow: 'public',
+          product: {
+            name: 'BC Address Geocoder',
+            organization: null,
+            organizationUnit: null,
+          },
+        },
+        plugins: [
+          { id: '25', name: 'statsd' },
+          { id: '24', name: 'response-transformer' },
+          { id: '33', name: 'acl' },
+          { id: '26', name: 'bcgov-gwa-endpoint' },
+          { id: '28', name: 'http-log' },
+          { id: '32', name: 'key-auth' },
+          { id: '30', name: 'cors' },
+          { id: '22', name: 'rate-limiting' },
+        ],
+        routes: [
+          {
+            id: '53',
+            name: 'apidata-geocoder-geocoder-geocoder-906eb-0',
+            hosts: '["geocoder.api.gov.bc.ca"]',
+            paths:
+              '["/addresses*","/sites/*","/intersections/*","/occupants/*"]',
+            methods: '["GET","OPTIONS"]',
+          },
+        ],
+        updatedAt: '2021-06-15T19:44:48.732Z',
+      },
+    },
+  };
+  const _routes = d.data.GatewayService.routes;
   return (
     <>
-      {routes.map((route) => {
+      {_routes.map((route) => {
         const _methods = JSON.parse(route['methods']);
         const methods =
           Array.isArray(_methods) && _methods.length > 0 ? _methods : ['ALL'];
@@ -19,16 +73,19 @@ const ServiceRoutes: React.FC<ServiceRoutesProps> = ({ routes }) => {
         const paths = Array.isArray(JSON.parse(route['paths']))
           ? JSON.parse(route['paths'])
           : ['/'];
-        const hostPaths = hosts.map((h) =>
-          paths.map((p) => `https://${h}${p}`)
-        );
+        const hostPaths = hosts
+          .map((h) => paths.map((p) => `https://${h}${p}`))
+          .flat();
         return (
-          <Stack direction="row">
+          <VStack
+            align="left"
+            divider={<StackDivider borderColor="gray.200" />}
+          >
             {hostPaths.map((hp) => (
-              <>
+              <SimpleGrid columns={1}>
                 <Stack
                   direction="row"
-                  wrap="wrap"
+                  wrap="nowrap"
                   spacing={1}
                   m={1}
                   shouldWrapChildren={true}
@@ -45,9 +102,9 @@ const ServiceRoutes: React.FC<ServiceRoutesProps> = ({ routes }) => {
                   ))}
                 </Stack>
                 <Box m={1}>{hp}</Box>
-              </>
+              </SimpleGrid>
             ))}
-          </Stack>
+          </VStack>
         );
       })}
     </>
