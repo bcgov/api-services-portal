940e76ddbbd15cab12b96c082c2a6cc714340034
Joshua Jones <joshua@general-metrics.com>
2021-06-18 14:35:13 -0700

    Move 90% of namespace access into 1 file.

--- src/nextapp/pages/manager/namespace-access/index.tsx
+++ src/nextapp/pages/manager/namespace-access/index.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import { gql } from 'graphql-request';
-import ResourceAccess from './resource-access';
 import api, { useApi } from '@/shared/services/api';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { QueryClient } from 'react-query';
 import { Query } from '@/shared/types/query.types';
 import { dehydrate } from 'react-query/hydration';
+import { Box, Container, Divider, Heading } from '@chakra-ui/react';
+import GrantAccessDialog from '@/components/grant-access-dialog';
+import GrantServiceAccountDialog from '@/components/grant-service-account-dialog';
+import PageHeader from '@/components/page-header';
+import Head from 'next/head';
+import ResourcesManager from '@/components/resources-manager';
+import { useAuth } from '@/shared/services/auth';
+import EmptyPane from '@/components/empty-pane';
+import UsersAccessList from '@/components/users-access-list';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const queryKey = 'namespaceAccess';
@@ -30,7 +38,15 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 const AccessRedirectPage: React.FC<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ queryKey }) => {
-  const { data } = useApi(
+  const { user } = useAuth();
+  const breadcrumbs = user
+    ? [
+        { href: '/manager/namespaces', text: 'Namespaces' },
+        { href: '/manager/namespaces', text: user.namespace },
+      ]
+    : [];
+
+  const { data, isSuccess } = useApi(
     queryKey,
     { query },
     {
@@ -38,14 +54,99 @@ const AccessRedirectPage: React.FC<
     }
   );
 
+  const resourceId = data?.currentNamespace?.id;
+  const prodEnvId = data?.currentNamespace?.prodEnvId;
+
+  const permissions = useApi(
+    'namespacePermissions',
+    {
+      query: permissionsQuery,
+      variables: {
+        resourceId,
+        prodEnvId,
+      },
+    },
+    {
+      enabled: Boolean(data) && isSuccess,
+    }
+  );
+  const requests = permissions.data?.getPermissionTicketsForResource.filter(
+    (p) => !p.granted
+  );
+
   return (
-    <ResourceAccess
-      queryKey=""
-      variables={{
-        resourceId: data?.currentNamespace?.id,
-        prodEnvId: data?.currentNamespace?.prodEnvId,
-      }}
-    />
+    <>
+      <Head>
+        <title>{`API Program Services | Resources | ${permissions.data?.getResourceSet.type} ${permissions.data?.getResourceSet.name}`}</title>
+      </Head>
+      <Container maxW="6xl">
+        <PageHeader
+          actions={
+            permissions.isSuccess &&
+            requests.length > 0 && (
+              <ResourcesManager
+                data={requests}
+                resourceId={resourceId}
+                prodEnvId={prodEnvId}
+                queryKey={queryKey}
+              />
+            )
+          }
+          breadcrumb={breadcrumbs}
+          title="Namespace Access"
+        />
+        <Box bgColor="white" my={4} mb={4}>
+          <Box
+            p={4}
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Heading size="md">Users with Access</Heading>
+            <GrantAccessDialog
+              prodEnvId={prodEnvId}
+              resource={permissions.data?.getResourceSet}
+              resourceId={resourceId}
+              queryKey={queryKey}
+            />
+          </Box>
+          <Divider />
+          {!resourceId && (
+            <EmptyPane
+              message="This Service Access Request contains no resources"
+              title="No Resources"
+            />
+          )}
+          <UsersAccessList
+            enableRevoke
+            data={permissions.data?.getPermissionTicketsForResource.filter(
+              (p) => p.granted
+            )}
+            resourceId={resourceId}
+            prodEnvId={prodEnvId}
+            queryKey={queryKey}
+          />
+        </Box>
+
+        <Box bgColor="white" my={4} mb={4}>
+          <Box
+            p={4}
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Heading size="md">Service Accounts with Access</Heading>
+            <GrantServiceAccountDialog
+              prodEnvId={prodEnvId}
+              resource={permissions.data?.getResourceSet}
+              resourceId={resourceId}
+              queryKey={queryKey}
+            />
+          </Box>
+          <Divider />
+        </Box>
+      </Container>
+    </>
   );
 };
 
@@ -60,6 +161,56 @@ const query = gql`
         name
       }
       prodEnvId
+    }
+  }
+`;
+
+const permissionsQuery = gql`
+  query GetPermissions($resourceId: String!, $prodEnvId: ID!) {
+    getPermissionTicketsForResource(
+      prodEnvId: $prodEnvId
+      resourceId: $resourceId
+    ) {
+      id
+      owner
+      ownerName
+      requester
+      requesterName
+      resource
+      resourceName
+      scope
+      scopeName
+      granted
+    }
+
+    getUmaPoliciesForResource(prodEnvId: $prodEnvId, resourceId: $resourceId) {
+      id
+      name
+      description
+      type
+      logic
+      decisionStrategy
+      owner
+      clients
+      users
+      scopes
+    }
+
+    getResourceSet(prodEnvId: $prodEnvId, resourceId: $resourceId) {
+      id
+      name
+      type
+      resource_scopes {
+        name
+      }
+    }
+
+    Environment(where: { id: $prodEnvId }) {
+      name
+      product {
+        id
+        name
+      }
     }
   }
 `;
