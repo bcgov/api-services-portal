b4012426306b45be36423b4da580bd2b4edb8e17
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2021-07-05 14:35:47 -0700

    Added feeder URL to env file

--- local/feeder-init/init.sh
+++ local/feeder-init/init.sh
@@ -9,7 +9,8 @@ while true; do
     if [[ "$keycloakstatus" == "200" ]]; then
         echo  "Keycloak is up"
         curl http://feeder.localtest.me:6000/push -F yaml=@legal.yaml
-        curl http://feeder.localtest.me:6000/push -F yaml=@user.yaml
+        curl http://feeder.localtest.me:6000/push -F yaml=@idir-user.yaml
+        curl http://feeder.localtest.me:6000/push -F yaml=@developer-user.yaml
         curl http://feeder.localtest.me:6000/push -F yaml=@platform-authz-profile.yaml
         curl http://feeder.localtest.me:6000/push -F yaml=@platform-gwa-api.yaml
         break
