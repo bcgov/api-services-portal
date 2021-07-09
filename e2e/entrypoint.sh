f5f2d2c2e90be1fc1c8c207af3aa20d81b877e01
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2021-07-09 13:41:28 -0700

    Added cypress as a service to run the e2e tests

---
+++ e2e/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /tmp
+
+while true; do
+    keycloakstatus=$(curl -o /dev/null -Isw '%{http_code}\n' http://keycloak.localtest.me:9080/auth/realms/master)
+    echo "$keycloakstatus"
+    if [[ "$keycloakstatus" == "200" ]]; then
+        echo  "Keycloak is up"
+        cd /e2e
+        npm run cy:run
+        break
+    else
+        echo  "Waiting for Keycloak....."
+        sleep 2m
+    fi
+done
+
+
