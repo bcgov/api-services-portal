07e74146822e28193bb0656f7813614cc1815148
ikethecoder <ikethecoder@canzea.com>
2021-08-05 19:14:28 -0700

    tweak ghaction

--- .github/workflows/scripts/init.sh
+++ .github/workflows/scripts/init.sh
@@ -9,13 +9,13 @@ while true; do
     echo "$status"
     if [[ "$status" == "200" ]]; then
         echo  "Portal is up"
-        kubectl port-forward service/${SERVICE} 8080:80 &
+        nohup kubectl port-forward service/${SERVICE} 8080:80 &
         FWD_PID=$!
         echo "Port forwarded ${SERVICE} with $FWD_PID"
         curl --fail http://localhost:8080/push -F yaml=@legal.yaml
         curl --fail http://localhost:8080/push -F yaml=@platform-authz-profile.yaml
         curl --fail http://localhost:8080/push -F yaml=@platform-gwa-api.yaml
-        kill -9 $FWD_PID
+        kill $FWD_PID
         break
     else
         echo  "Waiting for Portal....."
