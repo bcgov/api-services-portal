ce90b6d1e1bf314441596e1256833ecef6e4ee5b
ikethecoder <ikethecoder@canzea.com>
2021-08-05 19:01:10 -0700

    tweak ghaction

--- .github/workflows/scripts/init.sh
+++ .github/workflows/scripts/init.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#/bin/bash -e
 
 python scripts/template.py scripts/feeder-init/legal.yaml legal.yaml
 python scripts/template.py scripts/feeder-init/platform-authz-profile.yaml platform-authz-profile.yaml
