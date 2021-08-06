a799c988e5c87e1e05869283b44431b84507a6c9
ikethecoder <ikethecoder@canzea.com>
2021-08-05 18:55:58 -0700

    upd build

---
+++ .github/workflows/scripts/init.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+python scripts/template.py scripts/feeder-init/legal.yaml legal.yaml)
+python scripts/template.py scripts/feeder-init/platform-authz-profile.yaml platform-authz-profile.yaml
+python scripts/template.py scripts/feeder-init/platform-gwa-api.yaml platform-gwa-api.yaml
+
+while true; do
+    status=$(curl -o /dev/null -Isw '%{http_code}\n' ${PORTAL_URL}/health)
+    echo "$status"
+    if [[ "$status" == "200" ]]; then
+        echo  "Portal is up"
+        kubectl port-forward service/${SERVICE} 8080:80 &
+        FWD_PID=$!
+        curl http://localhost:8080/push -F yaml=@legal.yaml
+        curl http://localhost:8080/push -F yaml=@platform-authz-profile.yaml
+        curl http://localhost:8080/push -F yaml=@platform-gwa-api.yaml
+        kill -9 $FWD_PID
+        break
+    else
+        echo  "Waiting for Portal....."
+        sleep 1m
+    fi
+done
