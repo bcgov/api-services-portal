1ea4f501ef4296d11a4dc0cf9809b5d9e32b99c2
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2021-08-24 18:39:41 -0700

    Added new test to replicate developer application creation and access request process

---
+++ e2e/cypress/pageObjects/apiDirectory.ts
@@ -0,0 +1,19 @@
+class ApiDirectoryPage {
+  path: string = '/devportal/api-directory'
+  rqstAccessBtn: string = '[data-testid=api-rqst-access-btn]'
+  appSelect: string = '[data-testid=access-rqst-app-select]'
+  additionalNotes: string = '[data-testid=access-rqst-add-notes-text]'
+  submitBtn: string = '[data-testid=access-rqst-submit-btn]'
+
+  createAccessRequest(product: any, app: any, accessRqst: any) {
+    cy.contains(product.name).click()
+    cy.get(this.rqstAccessBtn).click()
+    cy.get(this.appSelect).select(app.name)
+    cy.get('[data-testid=access-rqst-app-env-' + product.environment + ']').click()
+    cy.get(this.additionalNotes).type(accessRqst.notes)
+    cy.contains('Terms of Use for API Gateway').click()
+    cy.get(this.submitBtn).click()
+  }
+}
+
+export default ApiDirectoryPage
