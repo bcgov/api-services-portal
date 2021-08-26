04d0f065f160adcabb275babff3fc9fb3de5a041
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2021-08-26 16:58:07 -0700

    Updated products id page with try catch to handle 500 errors when product not found after login

--- src/nextapp/pages/manager/products/[id].tsx
+++ src/nextapp/pages/manager/products/[id].tsx
@@ -14,6 +14,7 @@ import EnvironmentPlugins from '@/components/environment-plugins';
 import EnvironmentConfig from '@/components/environment-config';
 import EnvironmentNav from '@/components/environment-nav';
 import { dehydrate } from 'react-query/hydration';
+import Error from 'next/error';
 
 import breadcrumbs from '@/components/ns-breadcrumb';
 
@@ -43,65 +44,74 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 const EnvironmentPage: React.FC<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ id }) => {
-  const { user } = useAuth();
-  const { data } = useApi(['environment', id], {
-    query: GET_ENVIRONMENT,
-    variables: { id },
-  });
-  const title = `${data.Environment?.product.name} Environment`;
-  const breadcrumb = [
-    { href: '/manager/products', text: 'Products' },
-    {
-      text: title,
-    },
-  ];
+  try {
+    const { user } = useAuth();
+    const { data } = useApi(['environment', id], {
+      query: GET_ENVIRONMENT,
+      variables: { id },
+    });
+    const title = `${data.Environment?.product.name} Environment`;
+    const breadcrumb = [
+      { href: '/manager/products', text: 'Products' },
+      {
+        text: title,
+      },
+    ];
 
-  return (
-    <>
-      <Head>
-        <title>API Program Services | {title}</title>
-      </Head>
-      <Container maxW="6xl">
-        <PageHeader
-          actions={
-            <EnvironmentNav
-              id={data.Environment?.id}
-              data={data.Environment?.product?.environments}
-            />
-          }
-          breadcrumb={breadcrumbs([
-            { href: '/manager/products', text: 'Products' },
-            { text: title },
-          ])}
-          title={
+    return (
+      <>
+        <Head>
+          <title>API Program Services | {title}</title>
+        </Head>
+        <Container maxW="6xl">
+          <PageHeader
+            actions={
+              <EnvironmentNav
+                id={data.Environment?.id}
+                data={data.Environment?.product?.environments}
+              />
+            }
+            breadcrumb={breadcrumbs([
+              { href: '/manager/products', text: 'Products' },
+              { text: title },
+            ])}
+            title={
+              <>
+                Edit Environment{' '}
+                <Badge
+                  ml={1}
+                  fontSize="1rem"
+                  colorScheme="blue"
+                  variant="solid"
+                >
+                  {data.Environment?.name}
+                </Badge>
+              </>
+            }
+          >
             <>
-              Edit Environment{' '}
-              <Badge ml={1} fontSize="1rem" colorScheme="blue" variant="solid">
-                {data.Environment?.name}
-              </Badge>
+              <Text></Text>
             </>
-          }
-        >
-          <>
-            <Text></Text>
-          </>
-        </PageHeader>
-        <Box>
-          <EnvironmentConfig data={data.Environment} />
-          <EnvironmentPlugins data={data.Environment} />
-        </Box>
-        <Box my={5}>
-          {user?.namespace && (
-            <ServicesManager
-              data={data.Environment?.services}
-              environmentId={id}
-              namespace={user.namespace}
-            />
-          )}
-        </Box>
-      </Container>
-    </>
-  );
+          </PageHeader>
+          <Box>
+            <EnvironmentConfig data={data.Environment} />
+            <EnvironmentPlugins data={data.Environment} />
+          </Box>
+          <Box my={5}>
+            {user?.namespace && (
+              <ServicesManager
+                data={data.Environment?.services}
+                environmentId={id}
+                namespace={user.namespace}
+              />
+            )}
+          </Box>
+        </Container>
+      </>
+    );
+  } catch (err) {
+    return <Error statusCode={500} />;
+  }
 };
 
 export default EnvironmentPage;
