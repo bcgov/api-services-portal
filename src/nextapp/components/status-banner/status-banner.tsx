b53b6231c7ca57399b20fb4bfbcac798e5fb5f52
Justin Tendeck <justin.tendeck@gmail.com>
2021-09-22 09:48:46 -0700

    Enlargen, bold status banner title for demo

--- src/nextapp/components/status-banner/status-banner.tsx
+++ src/nextapp/components/status-banner/status-banner.tsx
@@ -30,7 +30,7 @@ const StatusBanner: React.FC<StatusBannerProps> = ({
       <AlertIcon />
       <Box flex="1">
         {title && (
-          <AlertTitle mr={2} fontWeight="normal">
+          <AlertTitle mr={2}  fontWeight={600} fontSize='xl'>
             {title}
           </AlertTitle>
         )}
