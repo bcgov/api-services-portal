1a263fdf5db93303f91a9468e686d7f79a873ad5
ikethecoder <ikethecoder@canzea.com>
2021-11-02 16:43:55 -0700

    upd identifiers

--- src/services/identifiers.ts
+++ src/services/identifiers.ts
@@ -1,9 +1,21 @@
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
 
-export function isProductID (id: string) : boolean { return id.length == 12 }
-export function isApplicationID (id: string) : boolean { return id.length == 16 }
-export function isEnvironmentID (id: string) : boolean { return id.length == 8 }
+export function isProductID(id: string): boolean {
+  return id.length == 12;
+}
+export function isApplicationID(id: string): boolean {
+  return id.length == 11 || id.length == 16;
+}
+export function isEnvironmentID(id: string): boolean {
+  return id.length == 8;
+}
 
-export function newProductID () : string { return uuidv4().replace(/-/g,'').toUpperCase().substr(0, 12) }
-export function newApplicationID () : string { return uuidv4().replace(/-/g,'').toUpperCase().substr(0, 16) }
-export function newEnvironmentID () : string { return uuidv4().replace(/-/g,'').toUpperCase().substr(0, 8) }
+export function newProductID(): string {
+  return uuidv4().replace(/-/g, '').toUpperCase().substr(0, 12);
+}
+export function newApplicationID(): string {
+  return uuidv4().replace(/-/g, '').toUpperCase().substr(0, 11);
+}
+export function newEnvironmentID(): string {
+  return uuidv4().replace(/-/g, '').toUpperCase().substr(0, 8);
+}
