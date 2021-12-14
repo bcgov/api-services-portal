7432173e06d3a285f180a7f2e6ff32293192c0af
Joshua Jones <joshua@general-metrics.com>
2021-12-13 16:47:00 -0800

    Add user profile and story

---
+++ src/nextapp/components/user-profile/user-profile.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import {
+  Avatar,
+  Box,
+  Flex,
+  Heading,
+  Skeleton,
+  SkeletonCircle,
+  Text,
+} from '@chakra-ui/react';
+import { UserData } from '@/shared/types/app.types';
+
+interface UserProfileProps {
+  data?: UserData;
+  isLoading?: boolean;
+}
+
+const UserProfile: React.FC<UserProfileProps> = ({
+  data = {},
+  isLoading = false,
+}) => {
+  return (
+    <Box>
+      <Heading size="sm" mb={3.5}>
+        Administrator:
+      </Heading>
+      <Flex>
+        <Avatar name={data.name} />
+        <Flex ml={2.5} flex={1} direction="column" justify="space-between">
+          {isLoading && (
+            <>
+              <Flex align="center">
+                <Skeleton width="20%" height="20px" />
+                <SkeletonCircle size="2" mx={2} />
+                <Skeleton width="20%" height="20px" />
+              </Flex>
+              <Skeleton width="35%" height="20px" />
+            </>
+          )}
+          {!isLoading && (
+            <>
+              <Text lineHeight="1">
+                <Text as="span" fontWeight="bold">
+                  {data.name}
+                </Text>
+                <Text as="span" color="bc-component">
+                  <Text as="span" mx={1}>
+                    &bull;
+                  </Text>
+                  {data.username}
+                </Text>
+              </Text>
+              <Text color="bc-component">{data.email}</Text>
+            </>
+          )}
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default UserProfile;
