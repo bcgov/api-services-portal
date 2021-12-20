dcd50f796037e47e0ee85f7ba2292d4467ad8439
Justin Tendeck <justin.tendeck@goc.bc.ca>
2021-12-20 15:03:07 -0800

    Finishes up CC tests

--- e2e/cypress/tests/01-create-api.spec.ts
+++ e2e/cypress/tests/01-create-api.spec.ts
@@ -70,7 +70,6 @@ describe('Create API Spec', () => {
       })
     })
   })
-
   it('update the Dataset in BC Data Catelogue to appear the API in the Directory', () => {
 
     cy.visit(pd.path)
--- e2e/cypress/tests/05-client-credential-flow.spec.ts
+++ e2e/cypress/tests/05-client-credential-flow.spec.ts
@@ -45,85 +45,94 @@ describe('Client Credential Flow', () => {
     cy.visit(login.path)
   })
 
-  // it('Logs in as API Owner', () => {
-  //   cy.get('@apiowner').then(({ user, namespace }: any) => {
-  //     cy.login(user.credentials.username, user.credentials.password)
-  //     home.useNamespace(namespace)
-  //   })
-  // })
+  it('Logs in as API Owner', () => {
+    cy.get('@apiowner').then(({ user, namespace }: any) => {
+      cy.login(user.credentials.username, user.credentials.password)
+      home.useNamespace(namespace)
+    })
+  })
 
-  // it('Gives API Owner CredentialIssuer.Admin and Access.Manage permission', () => {
-  //   cy.visit(nsa.path)
-  //   cy.get('@apiowner').then(({ user, namespace, namespaceAccessPermissions }: any) => {
-  //     nsa.clickGrantUserAccessButton()
-  //     nsa.grantPermission({userName: user.credentials.username, accessRole: namespaceAccessPermissions})
-  //     cy.visit('/').then(() => {
-  //       // TODO there appears to be some flakiness with this logging in and logging out step
-  //       cy.logout()
-  //       cy.login(user.credentials.username, user.credentials.password)
-  //       home.useNamespace(namespace)
-  //     })
-  //   })
-  // })
+  it('Gives API Owner CredentialIssuer.Admin and Access.Manage permission', () => {
+    cy.visit(nsa.path)
+    cy.get('@apiowner').then(({ user, namespace, namespaceAccessPermissions }: any) => {
+      nsa.clickGrantUserAccessButton()
+      nsa.grantPermission({userName: user.credentials.username, accessRole: namespaceAccessPermissions})
+      cy.visit('/').then(() => {
+        // TODO there appears to be some flakiness with this logging in and logging out step
+        cy.logout()
+        cy.login(user.credentials.username, user.credentials.password)
+        home.useNamespace(namespace)
+      })
+    })
+  })
 
-  // it('Creates Auth Profile', () => {
-  //   cy.visit(authProfile.path)
-  //   cy.get('@apiowner').then(({ ccAuthProfile }: any) => {
-  //     authProfile.createAuthProfile(ccAuthProfile)
-  //     cy.get(authProfile.profileTable).contains(ccAuthProfile.name).should('be.visible')
-  //   })
+  it('Creates Auth Profile', () => {
+    cy.visit(authProfile.path)
+    cy.get('@apiowner').then(({ ccAuthProfile }: any) => {
+      authProfile.createAuthProfile(ccAuthProfile)
+      cy.get(authProfile.profileTable).contains(ccAuthProfile.name).should('be.visible')
+    })
     
-  //   cy.log('profile created!')
-  // })
+    cy.log('profile created!')
+  })
 
-  // it('Adds Test environment to "Auto Test Product" product', () => {
-  //   cy.visit(products.path)
-  //   cy.get('@apiowner').then(({ product }: any) => {
-  //     products.addEnvToProduct(product.name, product.testEnvironment.name)
-  //   })
-  // })
+  it('Adds Test environment to "Auto Test Product" product', () => {
+    cy.visit(products.path)
+    cy.get('@apiowner').then(({ product }: any) => {
+      products.addEnvToProduct(product.name, product.testEnvironment.name)
+    })
+  })
 
-  // it('Adds Client Credential Flow to Test environment', () => {
-  //   cy.visit(products.path)
-  //   cy.get('@apiowner').then(({ product, ccAuthProfile }: any) => {
-  //     products.editProductEnvironment(product.name, product.testEnvironment.name)
-  //     product.testEnvironment.config.authIssuer = ccAuthProfile.name
-  //     product.testEnvironment.config.authIssuerEnv = ccAuthProfile.environmentConfig.environment
-  //     products.editProductEnvironmentConfig(product.testEnvironment.config)
-  //   })
-  // })
+  it('Adds Client Credential Flow to Test environment, generates template with jwt-keycloak plugin', () => {
+    cy.visit(products.path)
+    cy.get('@apiowner').then(({ product, ccAuthProfile }: any) => {
+      products.editProductEnvironment(product.name, product.testEnvironment.name)
+      product.testEnvironment.config.authIssuer = ccAuthProfile.name
+      product.testEnvironment.config.authIssuerEnv = ccAuthProfile.environmentConfig.environment
+      products.editProductEnvironmentConfig(product.testEnvironment.config)
+    })
+    products.generateKongPluginConfig()
+  })
 
-  // it('API Owner logs out', () => {
-  //   cy.logout()
-  // })
+  it('applies authorization plugin to service published to Kong Gateway', () => {
+    cy.publishApi('service-plugin.yml').then(() => {
+      cy.get('@publishAPIResponse').then((res: any) => {
+        cy.log(JSON.stringify(res.body))
+      })
+    })
+  })
 
-  // it('Developer logs in', () => {
-  //   cy.get('@developer').then(({ user, namespace }: any) => {
-  //     cy.login(user.credentials.username, user.credentials.password)
-  //     // home.useNamespace(namespace)
-  //   })
-  // })
+  it('API Owner logs out', () => {
+    cy.logout()
+  })
 
-  // it('creates an application', () => {
-  //   cy.visit(app.path)
-  //   cy.get('@developer').then(({ clientCredentialsApplication }: any) => {
-  //     app.createApplication(clientCredentialsApplication)
-  //   })
-  // })
+  it('Developer logs in', () => {
+    cy.get('@developer').then(({ user, namespace }: any) => {
+      cy.login(user.credentials.username, user.credentials.password)
+      // home.useNamespace(namespace)
+    })
+  })
 
-  // it('creates an access request', () => {
-  //   cy.visit(apiDir.path)
-  //   cy.get('@developer').then(({ product, clientCredentialsApplication, accessRequest }: any) => {
-  //     product.environment = 'test';
-  //     apiDir.createAccessRequest(product, clientCredentialsApplication, accessRequest)
-  //     sa.checkClientCredentialsVisible();
-  //     sa.saveClientCredentials();
-  //   })
-  // })
+  it('creates an application', () => {
+    cy.visit(app.path)
+    cy.get('@developer').then(({ clientCredentialsApplication }: any) => {
+      app.createApplication(clientCredentialsApplication)
+    })
+  })
 
-  // it('developer logs out', () => {
-  //   cy.logout();
-  // })
+  it('creates an access request', () => {
+    cy.visit(apiDir.path)
+    cy.get('@developer').then(({ product, clientCredentialsApplication, accessRequest }: any) => {
+      product.environment = 'test';
+      apiDir.createAccessRequest(product, clientCredentialsApplication, accessRequest)
+      sa.checkClientCredentialsVisible();
+      sa.saveClientCredentials();
+    })
+  })
+
+  it('developer logs out', () => {
+    cy.logout();
+  })
 
   it('API Owner Logs in', () => {
     cy.get('@apiowner').then(({ user, namespace }: any) => {
@@ -143,13 +152,24 @@ describe('Client Credential Flow', () => {
     })
   })
 
-  it('Gets client credential access token, make API request', () => {
+  it('Gets access token using client ID and secret; make API request', () => {
     cy.readFile('cypress/fixtures/state/store.json').then((res) => {
       let cc = JSON.parse(res.clientCredentials);
       cy.getAccessToken(cc.clientId, cc.clientSecret).then(() => {
         cy.get('@accessTokenResponse').then((res : any) => {
           let token = res.body.access_token
           cy.log(token)
+          cy.request({
+            url: Cypress.env('KONG_URL'),
+            headers: {
+              Host: 'a-service-for-platform.api.gov.bc.ca'
+            },
+            auth: {
+              bearer: token
+            },
+          }).then((res) => {
+            expect(res.status).to.eq(200)
+          })
         })
       });
     })
