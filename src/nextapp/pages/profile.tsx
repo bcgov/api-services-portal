a462ba27bd2ebeeb62776f1c759625c2ad29b664
Joshua Jones <joshua@general-metrics.com>
2022-01-25 13:52:48 -0800

    Add roles.

--- src/nextapp/pages/profile.tsx
+++ src/nextapp/pages/profile.tsx
@@ -10,6 +10,7 @@ import {
   Heading,
   Text,
 } from '@chakra-ui/react';
+import Head from 'next/head';
 import { uid } from 'react-uid';
 
 const fields = [
@@ -22,24 +23,35 @@ const MyProfilePage: React.FC = () => {
   const { user } = useAuth();
 
   return (
-    <Container maxW="6xl">
-      <Box as="header" mt={12} mb={6}>
-        <Heading>Your Profile</Heading>
-      </Box>
-      <Flex as="article" bgColor="white" p={12} align="center">
-        <Avatar name={user.name} size="xl" mr={12} />
-        <Grid flex={1} templateColumns="224px 1fr" gridRowGap={8}>
-          {fields.map((f) => (
-            <GridItem key={uid(f)}>
+    <>
+      <Head>
+        <title>API Program Services | Profile</title>
+      </Head>
+      <Container maxW="6xl">
+        <Box as="header" mt={12} mb={6}>
+          <Heading>Your Profile</Heading>
+        </Box>
+        <Flex as="article" bgColor="white" p={12} align="center">
+          <Avatar name={user.name} size="xl" mr={12} />
+          <Grid flex={1} templateColumns="224px 1fr" gridRowGap={8}>
+            {fields.map((f) => (
+              <GridItem key={uid(f)}>
+                <Text color="bc-component" opacity={0.6}>
+                  {f.name}
+                </Text>
+                <Text>{user[f.key] ?? '-'}</Text>
+              </GridItem>
+            ))}
+            <GridItem>
               <Text color="bc-component" opacity={0.6}>
-                {f.name}
+                Roles
               </Text>
-              <Text>{user[f.key]}</Text>
+              <Text>{user?.roles.join(', ')}</Text>
             </GridItem>
-          ))}
-        </Grid>
-      </Flex>
-    </Container>
+          </Grid>
+        </Flex>
+      </Container>
+    </>
   );
 };
 
