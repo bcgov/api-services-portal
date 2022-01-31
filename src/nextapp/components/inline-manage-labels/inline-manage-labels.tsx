d121dfdc9b0e72ee9107f426d517c4db8a5cf30c
Joshua Jones <joshua@general-metrics.com>
2022-01-30 23:24:40 -0800

    First pass at new consumers table.

---
+++ src/nextapp/components/inline-manage-labels/inline-manage-labels.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import {
+  Box,
+  Icon,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { HiOutlineChevronDown } from 'react-icons/hi';
+import LabelDialog from './label-dialog';
+
+const InlineManageLabels: React.FC = () => {
+  const { isOpen, onClose, onToggle } = useDisclosure();
+
+  return (
+    <>
+      <LabelDialog isOpen={isOpen} onClose={onClose} />
+      <Box as="span" pos="relative">
+        Labels
+        <Menu>
+          <MenuButton pos="absolute" top="calc(-50% + 5px)">
+            <IconButton
+              aria-label="Manage labels button"
+              icon={<Icon as={HiOutlineChevronDown} />}
+              variant="ghost"
+            />
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={onToggle}>Manage Group Labels</MenuItem>
+          </MenuList>
+        </Menu>
+      </Box>
+    </>
+  );
+};
+
+export default InlineManageLabels;
