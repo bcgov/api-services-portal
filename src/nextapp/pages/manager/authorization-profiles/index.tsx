1363714e63300b6d0e5efdf4796d223a57f49450
Joshua Jones <joshua@general-metrics.com>
2022-02-03 09:29:03 -0800

    UI tweaks and update table.

--- src/nextapp/pages/manager/authorization-profiles/index.tsx
+++ src/nextapp/pages/manager/authorization-profiles/index.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import api, { useApi, useApiMutation } from '@/shared/services/api';
+import ActionsMenu from '@/components/actions-menu';
 import AuthorizationProfileForm from '@/components/authorization-profile-form';
 import {
   Box,
   Button,
   Container,
-  Divider,
+  Grid,
+  GridItem,
   Heading,
-  Table,
-  Thead,
-  Tbody,
   Tr,
-  Th,
   Td,
   Popover,
   PopoverTrigger,
@@ -25,23 +23,23 @@ import {
   Icon,
   MenuItem,
   useToast,
-  Grid,
-  Wrap,
-  WrapItem,
-  GridItem,
 } from '@chakra-ui/react';
 import { dehydrate } from 'react-query/hydration';
+import EmptyPane from '@/components/empty-pane';
 import { gql } from 'graphql-request';
 import Head from 'next/head';
 import PageHeader from '@/components/page-header';
 import { MdModeEditOutline } from 'react-icons/md';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { QueryClient, useQueryClient } from 'react-query';
-import { Mutation, Query } from '@/shared/types/query.types';
+import Table from '@/components/table';
 import { useNamespaceBreadcrumbs } from '@/shared/hooks';
-import ActionsMenu from '@/components/actions-menu';
-import EmptyPane from '@/components/empty-pane';
 import { useAuth } from '@/shared/services/auth';
+import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import type {
+  CredentialIssuer,
+  Mutation,
+  Query,
+} from '@/shared/types/query.types';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const queryKey = 'authorizationProfiles';
@@ -127,107 +125,107 @@ const AuthorizationProfiles: React.FC<
         </PageHeader>
         <Box bgColor="white" mb={4}>
           <Box
-            p={4}
+            py={6}
+            px={9}
             display="flex"
             alignItems="center"
             justifyContent="space-between"
           >
             <Heading size="md">All Profiles</Heading>
           </Box>
-          <Divider />
-          <Table variant="simple" data-testid="ap-all-profiles-table">
-            <Thead>
+          <Table
+            sortable
+            columns={[
+              { name: 'Name', key: 'name' },
+              { name: 'Flow', key: 'flow' },
+              { name: 'Mode', key: 'mode' },
+              { name: 'Owner', key: 'owner.name' },
+              { name: '', key: 'id' },
+            ]}
+            data={data.allCredentialIssuersByNamespace}
+            data-testid="ap-all-profiles-table"
+            emptyView={
               <Tr>
-                <Th>Name</Th>
-                <Th>Flow</Th>
-                <Th>Mode</Th>
-                <Th>Administrator</Th>
-                <Th />
+                <Td align="center" colspan={5}>
+                  <EmptyPane
+                    action={authorizationProfileForm}
+                    title="Create your first Authorization Profile"
+                    message={
+                      user?.namespace
+                        ? 'Manage authentication, authorization and clients access to your API'
+                        : 'Select a namespace first to view its profiles'
+                    }
+                  />
+                </Td>
               </Tr>
-            </Thead>
-            <Tbody>
-              {data.allCredentialIssuersByNamespace?.length === 0 && (
-                <Tr>
-                  <Td align="center" colspan={5}>
-                    <EmptyPane
-                      action={authorizationProfileForm}
-                      title="Create your first Authorization Profile"
-                      message={
-                        user?.namespace
-                          ? 'Manage authentication, authorization and clients access to your API'
-                          : 'Select a namespace first to view its profiles'
-                      }
-                    />
-                  </Td>
-                </Tr>
-              )}
-              {data.allCredentialIssuersByNamespace?.map((c) => (
-                <Tr key={c.id}>
-                  <Td>{c.name}</Td>
-                  <Td>{c.flow}</Td>
-                  <Td>{c.mode}</Td>
-                  <Td>
-                    <Popover isLazy trigger="hover">
-                      <PopoverTrigger>
-                        <Link color="bc-blue" fontWeight="bold">
-                          {c.owner?.name}
-                        </Link>
-                      </PopoverTrigger>
-                      <Portal>
-                        <PopoverContent
-                          border="1px solid"
-                          width="auto"
-                          borderColor="bc-component"
-                        >
-                          <PopoverArrow
-                            borderTop="1px solid"
-                            borderLeft="1px solid"
-                            borderColor="bc-component"
-                          />
-                          <PopoverBody
-                            d="grid"
-                            gridTemplateColumns="auto 1fr"
-                            gridGap={2}
-                            p={4}
-                          >
-                            <Avatar name={c.owner?.name} />
-                            <Grid
-                              as="dl"
-                              color="bc-component"
-                              columnGap={2}
-                              templateColumns="auto 1fr"
-                            >
-                              <GridItem as="dt" fontWeight="bold">
-                                Username:
-                              </GridItem>
-                              <GridItem as="dd">{c.owner?.name}</GridItem>
-                              <GridItem as="dt" fontWeight="bold" mr={1}>
-                                Email:
-                              </GridItem>
-                              <GridItem as="dd">{c.owner?.email}</GridItem>
-                            </Grid>
-                          </PopoverBody>
-                        </PopoverContent>
-                      </Portal>
-                    </Popover>
-                  </Td>
-                  <Td align="right">
-                    <AuthorizationProfileForm id={c.id} data={c}>
-                      <Button
-                        leftIcon={<Icon as={MdModeEditOutline} />}
-                        size="sm"
-                        variant="flat"
+            }
+          >
+            {(c: CredentialIssuer) => (
+              <Tr key={c.id}>
+                <Td>{c.name}</Td>
+                <Td>{c.flow}</Td>
+                <Td>{c.mode}</Td>
+                <Td>
+                  <Popover isLazy trigger="hover">
+                    <PopoverTrigger>
+                      <Link color="bc-blue" fontWeight="bold">
+                        {c.owner?.name}
+                      </Link>
+                    </PopoverTrigger>
+                    <Portal>
+                      <PopoverContent
+                        border="1px solid"
+                        width="auto"
+                        borderColor="bc-component"
                       >
-                        Edit
-                      </Button>
-                    </AuthorizationProfileForm>
-                    <ActionsMenu>
-                      <MenuItem onClick={handleDelete(c.id)}>Delete</MenuItem>
-                    </ActionsMenu>
-                  </Td>
-                </Tr>
-              ))}
-            </Tbody>
+                        <PopoverArrow
+                          borderTop="1px solid"
+                          borderLeft="1px solid"
+                          borderColor="bc-component"
+                        />
+                        <PopoverBody
+                          d="grid"
+                          gridTemplateColumns="auto 1fr"
+                          gridGap={2}
+                          p={4}
+                        >
+                          <Avatar name={c.owner?.name} />
+                          <Grid
+                            as="dl"
+                            color="bc-component"
+                            columnGap={2}
+                            templateColumns="auto 1fr"
+                          >
+                            <GridItem as="dt" fontWeight="bold">
+                              Username:
+                            </GridItem>
+                            <GridItem as="dd">{c.owner?.name}</GridItem>
+                            <GridItem as="dt" fontWeight="bold" mr={1}>
+                              Email:
+                            </GridItem>
+                            <GridItem as="dd">{c.owner?.email}</GridItem>
+                          </Grid>
+                        </PopoverBody>
+                      </PopoverContent>
+                    </Portal>
+                  </Popover>
+                </Td>
+                <Td textAlign="right">
+                  <AuthorizationProfileForm id={c.id} data={c}>
+                    <Button
+                      leftIcon={<Icon as={MdModeEditOutline} />}
+                      size="sm"
+                      variant="flat"
+                    >
+                      Edit
+                    </Button>
+                  </AuthorizationProfileForm>
+                  <ActionsMenu>
+                    <MenuItem onClick={handleDelete(c.id)}>Delete</MenuItem>
+                  </ActionsMenu>
+                </Td>
+              </Tr>
+            )}
           </Table>
         </Box>
       </Container>
