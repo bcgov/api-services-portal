6b5bf1f90509d291e3559653d81e06d6fd77fd75
ikethecoder <ikethecoder@canzea.com>
2022-05-13 17:33:45 -0700

    fix login flow

--- src/nextapp/components/login-buttons/login-buttons.tsx
+++ src/nextapp/components/login-buttons/login-buttons.tsx
@@ -1,13 +1,27 @@
 import * as React from 'react';
 import { Button, ButtonGroup } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import querystring from 'querystring';
+
+function buildUrl(hint: string, path: string) {
+  const redirect = `/admin/signin?${querystring.encode({
+    f: path,
+  })}`;
+
+  return `/oauth2/start?${querystring.encode({
+    kc_idp_hint: hint,
+    rd: redirect,
+  })}`;
+}
 
 const LoginButtons: React.FC = () => {
+  const router = useRouter();
   return (
     <ButtonGroup spacing={7}>
-      <Button as="a" variant="primary" href="/oauth2/start?kc_idp_hint=idir">
+      <Button as="a" variant="primary" href={buildUrl('idir', router?.asPath)}>
         IDIR
       </Button>
-      <Button as="a" variant="primary" href="/oauth2/start?kc_idp_hint=bceid">
+      <Button as="a" variant="primary" href={buildUrl('bceid', router?.asPath)}>
         BCeID
       </Button>
       <Button
@@ -16,7 +30,7 @@ const LoginButtons: React.FC = () => {
         bgColor="#f5f5f5"
         borderColor="#333"
         color="#333"
-        href="/oauth2/start?kc_idp_hint=github"
+        href={buildUrl('github', router?.asPath)}
       >
         Github
       </Button>
