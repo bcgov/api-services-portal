12b9b33c03e40c9bd472f39f098755d893320678
ikethecoder <ikethecoder@canzea.com>
2022-05-30 14:58:35 -0700

    minor fix for kube_cluster env var

--- src/nextapp/shared/config.ts
+++ src/nextapp/shared/config.ts
@@ -4,4 +4,4 @@ export const grafanaUrl = process.env.NEXT_PUBLIC_GRAFANA_URL || '';
 export const apiInternalHost = process.env.SSR_API_ROOT || '';
 export const appVersion = process.env.NEXT_PUBLIC_APP_VERSION ?? '';
 export const appRevision = process.env.NEXT_PUBLIC_APP_REVISION ?? '';
-export const appCluster = process.env.NEXT_PUBLIC_APP_CLUSTER ?? '';
+export const appCluster = process.env.NEXT_PUBLIC_KUBE_CLUSTER ?? '';
