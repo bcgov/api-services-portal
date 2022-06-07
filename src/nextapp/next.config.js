cfd9f402b6de818d062863654ce58a5ba2587657
ikethecoder <ikethecoder@canzea.com>
2022-06-07 10:16:33 -0700

    temp test for public envs v2

--- src/nextapp/next.config.js
+++ src/nextapp/next.config.js
@@ -4,6 +4,8 @@ module.exports = {
   distDir: `../${distDir}/www`,
   env: {
     USER_HAS_PORTFOLIO: !!process.env.IFRAMELY_API_KEY,
+    GHI: '123',
+    JKL: process.env.JKL,
   },
   publicRuntimeConfig: {
     ABC: '123',
