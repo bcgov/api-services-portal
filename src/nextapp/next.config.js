cdc6e44e2b87e9629cbbeeb2619a20a5e6cd4e1f
ikethecoder <ikethecoder@canzea.com>
2022-06-07 13:10:47 -0700

    upd next config

--- src/nextapp/next.config.js
+++ src/nextapp/next.config.js
@@ -6,9 +6,15 @@ module.exports = {
     USER_HAS_PORTFOLIO: !!process.env.IFRAMELY_API_KEY,
     GHI: '123',
     JKL: process.env.JKL,
+    NEXT_PUBLIC_KUBE_CLUSTER: process.env.NEXT_PUBLIC_KUBE_CLUSTER,
   },
   publicRuntimeConfig: {
     ABC: '123',
     DEF: process.env.DEF,
+    P: process.env.NEXT_PUBLIC_KUBE_CLUSTER,
+    NEXT_PUBLIC_KUBE_CLUSTER: process.env.NEXT_PUBLIC_KUBE_CLUSTER,
+    mix: {
+      helpLinks: [],
+    },
   },
 };
