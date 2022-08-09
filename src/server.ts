00f21b9b78878904f23a1d126483cb7b6b96f63e
ikethecoder <ikethecoder@canzea.com>
2022-08-09 13:38:29 -0700

    run as production when starting keystone

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 /// <reference types="node" />
 /// <reference types="express" />
 import 'reflect-metadata';
-import express from 'express';
-import request from 'graphql-request';
 const { Keystone } = require('@keystonejs/keystone');
-const { Checkbox, Password, Select } = require('@keystonejs/fields');
-//import Oauth2ProxyAuthStrategy from './auth/auth-oauth2-proxy'
 const { Oauth2ProxyAuthStrategy } = require('./auth/auth-oauth2-proxy');
 const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
 const { AdminUIApp } = require('@keystonejs/app-admin-ui');
 const { generate } = require('@graphql-codegen/cli');
-//const { AdminUIApp } = require('@keystone-next/admin-ui');
-const { StaticApp } = require('@keystonejs/app-static');
 const { NextApp } = require('@keystonejs/app-next');
 const { ApiProxyApp } = require('./api-proxy');
 const { ApiGraphqlWhitelistApp } = require('./api-graphql-whitelist');
@@ -20,19 +14,11 @@ const { MaintenanceApp } = require('./api-maintpage');
 const { ApiOpenapiApp } = require('./api-openapi');
 const { ApiDSProxyApp } = require('./api-proxy-ds');
 
-var Keycloak = require('keycloak-connect');
-
 const initialiseData = require('./initial-data');
-const { startAuthedSession } = require('@keystonejs/session');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
 
 const redis = require('redis');
 let RedisStore = require('connect-redis')(session);
-
-const { Strategy, Issuer, Client } = require('openid-client');
-
-const { staticRoute, staticPath, distDir } = require('./config');
 
 const {
   putFeedWorker,
@@ -41,10 +27,7 @@ const {
 } = require('./batch/feed-worker');
 const { Retry } = require('./services/tasked');
 
-const {
-  FieldEnforcementPoint,
-  EnforcementPoint,
-} = require('./authz/enforcement');
+const { EnforcementPoint } = require('./authz/enforcement');
 
 const { loadRulesAndWatch } = require('./authz/enforcement');
 
@@ -248,13 +231,6 @@ const authStrategy =
       });
 
 const { pages } = require('./admin-hooks.js');
-//const tasked = require('./services/tasked');
-
-const {
-  checkWhitelist,
-  loadWhitelistAndWatch,
-  addToWhitelist,
-} = require('./authz/whitelist');
 
 const apps = [
   new ApiHealthApp(state),
@@ -271,11 +247,6 @@ const apps = [
     authStrategy,
     pages: pages,
     enableDefaultRoute: false,
-    isAccessAllowed: (user: any) => {
-      // console.log('isAllowed?');
-      // console.log(JSON.stringify(user));
-      return true;
-    },
   }),
   new ApiDSProxyApp({ url: process.env.SSR_API_ROOT }),
   new ApiProxyApp({ gwaApiUrl: process.env.GWA_API_URL }),
