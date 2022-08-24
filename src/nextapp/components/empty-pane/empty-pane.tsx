6cc0ab2f0249918e5db473ed2b4781411025b43e
jTendeck <34200068+jTendeck@users.noreply.github.com>
2022-08-24 14:28:39 -0700

    API Provider - No Namespace Selected Page (#520)
    Co-authored-by: Justin Tendeck <justin.tendeck@goc.bc.ca>
    Co-authored-by: Joshua Jones <joshua@general-metrics.com>

--- src/nextapp/components/empty-pane/empty-pane.tsx
+++ src/nextapp/components/empty-pane/empty-pane.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
-import { Box, Center, Heading, Text } from '@chakra-ui/react';
+import {
+  Box,
+  BoxProps,
+  Center,
+  CenterProps,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
 
-interface EmptyPaneProps {
+interface EmptyPaneProps extends CenterProps {
   action?: React.ReactNode;
+  boxProps?: BoxProps;
+  children?: React.ReactNode;
   error?: string;
   message: string;
   title: string;
@@ -10,26 +19,31 @@ interface EmptyPaneProps {
 
 const EmptyPane: React.FC<EmptyPaneProps> = ({
   action,
+  boxProps = {},
+  children,
   error,
   message,
   title,
+  ...props
 }) => {
   return (
-    <Center my={6} data-testid="empty-pane">
+    <Center my={6} data-testid="empty-pane" width="100%" {...props}>
       <Box
         textAlign="center"
         py="6rem"
         px={8}
         bg="white"
         borderRadius="4px"
-        maxW={{ sm: 500 }}
         mx={{ base: 4 }}
+        width="100%"
+        {...boxProps}
       >
         <Heading as="h3" size="md" mb={2}>
           {title}
         </Heading>
         <Text>{message}</Text>
         {error && <Text color="red.500">{error}</Text>}
+        {children && <Box mt={6}>{children}</Box>}
         {action && <Box mt={6}>{action}</Box>}
       </Box>
     </Center>
--- src/nextapp/components/namespace-menu/namespace-menu.tsx
+++ src/nextapp/components/namespace-menu/namespace-menu.tsx
@@ -16,19 +16,23 @@ import { useQueryClient } from 'react-query';
 import { gql } from 'graphql-request';
 import { restApi, useApi } from '@/shared/services/api';
 import type { NamespaceData } from '@/shared/types/app.types';
-
 import NamespaceManager from '../namespace-manager';
 import NewNamespace from '../new-namespace';
 
 interface NamespaceMenuProps {
   user: UserData;
+  variant?: string;
+  buttonMessage?: string;
 }
 
-const NamespaceMenu: React.FC<NamespaceMenuProps> = ({ user }) => {
+const NamespaceMenu: React.FC<NamespaceMenuProps> = ({
+  user,
+  variant,
+  buttonMessage,
+}) => {
   const client = useQueryClient();
   const toast = useToast();
-  const newNamespaceDisclosure = useDisclosure();
-  const managerDisclosure = useDisclosure();
+  const { isOpen, onClose, onOpen } = useDisclosure();
   const { data, isLoading, isSuccess, isError } = useApi(
     'allNamespaces',
     { query },
@@ -63,20 +67,24 @@ const NamespaceMenu: React.FC<NamespaceMenuProps> = ({ user }) => {
     [client, toast]
   );
 
+  const isNamespaceSelector = variant === 'ns-selector';
+
   return (
     <>
       <Menu placement="bottom-end">
         <MenuButton
-          px={2}
-          py={1}
+          data-testid="ns-dropdown-btn"
+          px={isNamespaceSelector ? 5 : 2}
+          py={isNamespaceSelector ? 2 : 1}
           transition="all 0.2s"
           borderRadius={4}
-          _hover={{ bg: 'bc-link' }}
-          _expanded={{ bg: 'blue.400' }}
+          border={isNamespaceSelector ? '2px solid black' : ''}
+          borderColor={isNamespaceSelector ? 'bc-component' : ''}
+          _hover={isNamespaceSelector ? { boxShadow: 'md' } : { bg: 'bc-link' }}
+          _expanded={isNamespaceSelector ? {} : { bg: 'blue.400' }}
           _focus={{ boxShadow: 'outline' }}
-          data-testid="ns-dropdown-btn"
         >
-          {user?.namespace ?? 'No Active Namespace'}{' '}
+          {user?.namespace ?? buttonMessage ?? 'No Active Namespace'}{' '}
           <Icon as={FaChevronDown} ml={2} aria-label="chevron down icon" />
         </MenuButton>
         <MenuList
@@ -96,7 +104,9 @@ const NamespaceMenu: React.FC<NamespaceMenuProps> = ({ user }) => {
             )}
             {isSuccess && data.allNamespaces.length > 0 && (
               <>
-                <MenuOptionGroup title="Switch Namespace">
+                <MenuOptionGroup
+                  title={isNamespaceSelector ? '' : 'Switch Namespace'}
+                >
                   {data.allNamespaces
                     .filter((n) => n.name !== user.namespace)
                     .sort((a, b) => a.name.localeCompare(b.name))
@@ -110,38 +120,42 @@ const NamespaceMenu: React.FC<NamespaceMenuProps> = ({ user }) => {
                       </MenuItem>
                     ))}
                 </MenuOptionGroup>
-                <MenuDivider />
               </>
             )}
           </>
-          <MenuOptionGroup title="Namespace Actions">
-            <MenuItem
-              onClick={newNamespaceDisclosure.onOpen}
-              color="bc-blue-alt"
-              data-testid="ns-dropdown-create-btn"
-            >
-              Create New Namespace
-            </MenuItem>
-            <MenuItem
-              isDisabled={!data}
-              color="bc-blue-alt"
-              onClick={managerDisclosure.onOpen}
-              data-testid="ns-dropdown-manage-btn"
-            >
-              Export Namespace Report
-            </MenuItem>
-          </MenuOptionGroup>
+          {!isNamespaceSelector && (
+            <>
+              <MenuDivider />
+              <MenuOptionGroup title="Namespace Actions">
+                <MenuItem
+                  onClick={onOpen}
+                  color="bc-blue-alt"
+                  data-testid="ns-dropdown-create-btn"
+                >
+                  Create New Namespace
+                </MenuItem>
+                <MenuItem
+                  isDisabled={!data}
+                  color="bc-blue-alt"
+                  onClick={onOpen}
+                  data-testid="ns-dropdown-manage-btn"
+                >
+                  Export Namespace Report
+                </MenuItem>
+              </MenuOptionGroup>
+            </>
+          )}
         </MenuList>
       </Menu>
       <NewNamespace
-        isOpen={newNamespaceDisclosure.isOpen}
-        onClose={newNamespaceDisclosure.onClose}
+        isOpen={isOpen}
+        onClose={onClose}
       />
       {data && (
         <NamespaceManager
           data={data.allNamespaces}
-          isOpen={managerDisclosure.isOpen}
-          onClose={managerDisclosure.onClose}
+          isOpen={isOpen}
+          onClose={onClose}
         />
       )}
     </>
