2247434a35f4c959c7739a26cbd69c46d74046f8
Niraj Patel <niraj.patel@gov.bc.ca>
2022-09-19 14:11:51 -0700

    Added Function to convert the message

--- e2e/cypress/pageObjects/activity.ts
+++ e2e/cypress/pageObjects/activity.ts
@@ -4,11 +4,13 @@ class ActivityPage {
   clearAllBtn: string = '[data-testid="btn-filter-clear-all"]'
 
   checkActivityFilterForUser(user: string, response: any): void {
-    let responseText: string
+    let responseText: any
     let activityText: string
     this.setFilterCondition("User",user)
     var filteredResponse: any
     var activityCount: any
+    const para = document.querySelector('p');
+    debugger
     filteredResponse = Cypress._.filter(response, ["params.actor", user])
     // expect(cy.get('[data-testid^="activity-feed-heading-"]').nextAll('div').should('have.length', filteredResponse.length))
     // filteredResponse.forEach(function (accessResponse: any) {
@@ -18,6 +20,19 @@ class ActivityPage {
           cy.wrap($e1).find('strong').invoke('text').then((action) => {
             debugger
             activityText = actor + " " + action
+            responseText = filteredResponse[index].message
+            // responseText = responseText.replaceAll("{","`${filteredResponse[index].params.")
+            // responseText = responseText.replaceAll("}","}`")
+            responseText = responseText.replaceAll("{","${filteredResponse[index].params.")
+            // responseText = responseText.replaceAll("}","}`")
+            // responseText = `${responseText}`
+            const regexp = /\${([^{]+)}/g;
+            let result = responseText.replace(regexp, function(ignore:any, key:any){
+              return eval(key);
+          });
+            cy.log("Result -> "+result)  
+            responseText = "`"+responseText +"`"
+            responseText = `${filteredResponse[index].params.actor}-${filteredResponse[index].params.action}`
             responseText = filteredResponse[index].params.actor + " " + filteredResponse[index].params.action
             assert.equal(activityText,responseText)
           })
