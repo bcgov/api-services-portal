96487295413a83d7cd07f659f50bf29c21c137c1
jTendeck <justin.tendeck@gov.bc.ca>
2022-10-31 12:34:12 -0700

    fix 404 redirect

--- src/nextapp/pages/content/[slug].tsx
+++ src/nextapp/pages/content/[slug].tsx
@@ -2,6 +2,7 @@ import { Box, Container } from '@chakra-ui/react';
 import Head from 'next/head';
 import ReactMarkdown from 'react-markdown';
 import fs from 'fs/promises';
+import ErrorPage from 'next/error';
 import { join } from 'path';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
@@ -10,23 +11,34 @@ import styles from './content.module.css';
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { slug } = context.query;
   const contentDirectory = join(process.cwd(), 'nextapp', '_content');
-  
+
   try {
-    const content = await fs.readFile(join(contentDirectory, `${slug}.md`), 'utf8');
-    
+    const content = await fs.readFile(
+      join(contentDirectory, `${slug}.md`),
+      'utf8'
+    );
+
     return {
       props: {
         content,
-      }
+      },
     };
   } catch (err) {
-    context.res.writeHead(404, { location: '/404' }).end();
+    context.res.statusCode = 404
+    return {
+      props: {
+        content: null,
+      },
+    };
   }
 };
 
 const ContentPage: React.FC<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ content }) => {
+  if (!content) {
+    return <ErrorPage statusCode={404} />;
+  }
 
   return (
     <>
