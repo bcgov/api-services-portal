058eee7de4eb8eabd36943b7efab5c4284a3b794
Joshua Jones <joshua@general-metrics.com>
2023-01-05 11:39:22 -0800

    Update login dialog layout

--- src/nextapp/components/login-dialog/login-dialog.tsx
+++ src/nextapp/components/login-dialog/login-dialog.tsx
@@ -11,6 +11,7 @@ import {
   useDisclosure,
   ButtonProps,
   Flex,
+  VStack,
 } from '@chakra-ui/react';
 
 import LoginButtons from '../login-buttons';
@@ -27,7 +28,6 @@ const LoginDialog: React.FC<LoginDialogProps> = ({
 }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { identities, identityContent } = useGlobal();
-  const size = identities.developer.length > 2 ? '2xl' : 'lg';
   const isInline = buttonVariant === 'link';
   const buttonProps = !isInline
     ? {}
@@ -43,36 +43,25 @@ const LoginDialog: React.FC<LoginDialogProps> = ({
       <Button onClick={onOpen} variant={buttonVariant} {...buttonProps}>
         {buttonText}
       </Button>
-      <Modal isOpen={isOpen} onClose={onClose} size={size}>
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Login to request access</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Text mb={7}>
-              Choose which of the options you want to authenticate with. You
-              will go to a secure website to log in and automatically return.{' '}
-            </Text>
             <Text>
-              Access to certain features of the APS portal will be based on the
-              account type that you choose.
+              You will go to a secure website to log in and automatically
+              return.
             </Text>
           </ModalBody>
 
-          <Flex
-            justifyContent="flex-start"
-            gridGap={2}
-            px={8}
-            pt={4}
-            pb={6}
-            wrap="wrap"
-          >
+          <VStack justifyContent="stretch" gridGap={2} px={8} pt={5} pb={6}>
             <LoginButtons
               identities={identities.developer}
               identityContent={identityContent}
               variant="inline"
             />
-          </Flex>
+          </VStack>
         </ModalContent>
       </Modal>
     </>
