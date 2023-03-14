7ca3b86882df06c2cb27dce6ecd9c9766fd09431
ike thecoder <ikethecoder@copeconsulting.ca>
2023-03-13 23:01:00 -0700

    increase max records for query resources (#781)

--- src/test/integrated/keycloak/client.ts
+++ src/test/integrated/keycloak/client.ts
@@ -9,6 +9,7 @@ npm run ts-build
 export CID=""
 export CSC=""
 export ISSUER=""
+export TOK=""
 npm run ts-watch
 node dist/test/integrated/keycloak/client.js
 
@@ -17,14 +18,44 @@ node dist/test/integrated/keycloak/client.js
 import { o } from '../util';
 
 import { KeycloakClientService } from '../../../services/keycloak';
+import { UMAResourceRegistrationService } from '../../../services/uma2';
 
 (async () => {
-  const kc = new KeycloakClientService(process.env.ISSUER);
+  if (false) {
+    const kc = new KeycloakClientService(process.env.ISSUER);
 
-  await kc.login(process.env.CID, process.env.CSC);
+    await kc.login(process.env.CID, process.env.CSC);
 
-  // await kc.regenerateSecret('6af832cb-6178-438f-a4fc-8c5e1d14d5d2');
-  // console.log(await kc.listMembers('660cadef-9233-4532-ba45-5393beaddea4'));
-  const res = await kc.list('42da4f15');
-  o(res);
+    await kc.regenerateSecret('6af832cb-6178-438f-a4fc-8c5e1d14d5d2');
+    const res = await kc.list('42da4f15');
+    o(res);
+  }
+
+  const resourceRegistrationEndpoint =
+    process.env.ISSUER + '/authz/protection/resource_set';
+  const accessToken = process.env.TOK;
+  const clientUuid = '250398da-a174-404d-ae18-6a6ebc9de06d';
+
+  const kcprotectApi = new UMAResourceRegistrationService(
+    resourceRegistrationEndpoint,
+    accessToken
+  );
+  const resOwnerResourceIds = await kcprotectApi.listResources({
+    owner: clientUuid,
+    type: 'namespace',
+  });
+
+  const namespaces = await kcprotectApi.listResourcesByIdList(
+    resOwnerResourceIds
+  );
+
+  const matched = namespaces
+    .filter((ns) => ns.name == 'testelson')
+    .map((ns) => ({
+      id: ns.id,
+      name: ns.name,
+      scopes: ns.resource_scopes,
+    }));
+
+  o(matched);
 })();
