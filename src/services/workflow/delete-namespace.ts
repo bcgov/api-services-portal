df24602d62fefa22a152085467849ce21fa9e38d
ikethecoder <ikethecoder@copeconsulting.ca>
2023-07-26 01:30:47 -0700

    upd delete ns for issuers

--- src/services/workflow/delete-namespace.ts
+++ src/services/workflow/delete-namespace.ts
@@ -130,8 +130,8 @@ export const DeleteNamespace = async (
 
   const activity = await DeleteNamespaceRecordActivity(context, ns);
 
-  const gwaService = new GWAService(process.env.GWA_API_URL);
-  await gwaService.deleteAllGatewayConfiguration(subjectToken, ns);
+  //const gwaService = new GWAService(process.env.GWA_API_URL);
+  //await gwaService.deleteAllGatewayConfiguration(subjectToken, ns);
 
   const envs = await lookupEnvironmentsByNS(context, ns);
   const ids = envs.map((e: Environment) => e.id);
@@ -145,6 +145,10 @@ export const DeleteNamespace = async (
   ]);
 
   await deleteRecords(context, 'Product', { namespace: ns }, true, ['id']);
+
+  await deleteRecords(context, 'CredentialIssuer', { namespace: ns }, true, [
+    'id',
+  ]);
 
   await updateActivity(context.sudo(), activity.id, 'success', undefined);
 };
