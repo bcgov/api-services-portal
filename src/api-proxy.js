03ba6b21225d564d1e887ecd642246988e713972
ike thecoder <ikethecoder@copeconsulting.ca>
2024-06-20 11:51:45 -0700

    Update api-proxy to gwa-api to fix routing to v2

--- src/api-proxy.js
+++ src/api-proxy.js
@@ -17,7 +17,7 @@ class ApiProxyApp {
       logLevel: 'debug',
       pathRewrite: {
         '^/gw/api/v2/': '/v2/',
-        '^/gw/api/v3/': '/v2/',
+        '^/gw/api/v3/gateways/': '/v2/namespaces/'
       },
       onProxyReq: (proxyReq, req) => {
         //console.log(req.headers)
