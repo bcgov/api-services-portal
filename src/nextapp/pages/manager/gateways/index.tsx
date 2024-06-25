de2078aa68211771ae8cc57172f9594b4993f80b
James Elson <james.elson@gov.bc.ca>
2024-06-25 16:11:23 -0700

    Gateways redirect logic optimization

--- src/nextapp/pages/manager/gateways/index.tsx
+++ src/nextapp/pages/manager/gateways/index.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import { gql } from 'graphql-request';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { Container } from '@chakra-ui/react';
 
 import { useApi } from '@/shared/services/api';
-import useCurrentNamespace from '@/shared/hooks/use-current-namespace';
+import { useAuth } from '@/shared/services/auth';
+import PageHeader from '@/components/page-header';
+import { has } from 'lodash';
 
 const GatewaysHome: React.FC = () => {
   const { data, isSuccess, isError } = useApi(
@@ -11,36 +15,40 @@ const GatewaysHome: React.FC = () => {
     { query },
     { suspense: false }
   );
-  const namespace = useCurrentNamespace();
+  const { user } = useAuth();
+  const hasNamespace = !!user?.namespace;
   const router = useRouter();
+
   React.useEffect(() => {
-    if (isSuccess && data.allNamespaces.length === 0) {
-      router.push('/manager/gateways/get-started');
-    }
-    if (isSuccess && data.allNamespaces.length > 0) {
-      if (
-        namespace.isSuccess &&
-        !namespace.isFetching &&
-        namespace.data.currentNamespace
-      ) {
-        router.push('/manager/gateways/detail');
+    if (hasNamespace) {
+      router.push('/manager/gateways/detail');
+    } else {
+      if (isSuccess && data.allNamespaces.length === 0) {
+        router.push('/manager/gateways/get-started');
       }
-      if (
-        namespace.isSuccess &&
-        !namespace.isFetching &&
-        !namespace.data.currentNamespace
-      ) {
+      if (isSuccess && data.allNamespaces.length > 0) {
         router.push('/manager/gateways/list');
       }
-      if (namespace.isError) {
-        router.push('/manager/gateways/list');
+      if (isError) {
+        router.push('/manager/gateways/get-started');
       }
     }
-    if (isError) {
-      router.push('/manager/gateways/list');
-    }
-  }, [data, namespace]);
-  return <></>;
+  }, [data, hasNamespace]);
+
+  return (
+    <>
+      {!hasNamespace ? (
+        <>
+          <Head>
+            <title>API Program Services | My Gateways</title>
+          </Head>
+          <Container maxW="6xl">
+            <PageHeader title="My Gateways"></PageHeader>
+          </Container>
+        </>
+      ) : null}
+    </>
+  );
 };
 
 export default GatewaysHome;
