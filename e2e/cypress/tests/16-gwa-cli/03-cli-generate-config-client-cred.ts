539068539e79668fe098d2168418569639bdd778
Russell Vinegar <38586679+rustyjux@users.noreply.github.com>
2024-08-28 13:20:40 -0700

    Cypress/ns to gw cli tests (#1158)
    Cypress tests for new features in `gwa` CLI from NS to GW.

--- e2e/cypress/tests/16-gwa-cli/03-cli-generate-config-client-cred.ts
+++ e2e/cypress/tests/16-gwa-cli/03-cli-generate-config-client-cred.ts
@@ -5,10 +5,14 @@ import MyAccessPage from '../../pageObjects/myAccess'
 import HomePage from '../../pageObjects/home';
 import Products from '../../pageObjects/products';
 const YAML = require('yamljs');
-let userSession: any
 let cli = require("../../fixtures/test_data/gwa-cli.json")
-
+const { v4: uuidv4 } = require('uuid')
 const jose = require('node-jose')
+
+let userSession: any
+let namespace: string
+const customId = uuidv4().replace(/-/g, '').toLowerCase().substring(0, 3)
+const serviceName = 'my-service-' + customId
 
 describe('Verify CLI commands for generate/apply config', () => {
   const login = new LoginPage()
@@ -16,7 +20,6 @@ describe('Verify CLI commands for generate/apply config', () => {
   const app = new ApplicationPage()
   const ma = new MyAccessPage()
   const pd = new Products()
-  let namespace: string
   const home = new HomePage()
 
   before(() => {
@@ -45,26 +48,38 @@ describe('Verify CLI commands for generate/apply config', () => {
   })
 
   it('Check gwa command to generate config for client credential template', () => {
-    cy.executeCliCommand('gwa generate-config --template client-credentials-shared-idp --service my-service --upstream https://httpbin.org --org ministry-of-health --org-unit planning-and-innovation-division --out gw-config.yaml').then((response) => {
-      expect(response.stdout).to.contain("File gw-config.yaml created")
+    const serviceName = 'my-service-' + customId
+    const command = [
+      'gwa generate-config --template client-credentials-shared-idp',
+      `--service ${serviceName}`,
+      '--upstream https://httpbin.org',
+      '--org ministry-of-health',
+      '--org-unit planning-and-innovation-division',
+      '--out gw-config-cc.yaml'
+    ].join(' ');
+    cy.executeCliCommand(command).then((response) => {
+      expect(response.stdout).to.contain("File gw-config-cc.yaml created")
     });
   })
 
   it('Check gwa command to apply generated config', () => {
-    cy.executeCliCommand('gwa apply -i gw-config.yaml').then((response) => {
+    cy.executeCliCommand('gwa apply -i gw-config-cc.yaml').then((response) => {
+      expect(response.stdout).to.contain("4/4 Published, 0 Skipped")
       let wordOccurrences = (response.stdout.match(/\bcreated\b/g) || []).length;
       expect(wordOccurrences).to.equal(3)
-      namespace = response.stdout.match(/\bgw-\w+/g)[0]
     });
   })
 
-  it('activates new namespace', () => {
-    cy.activateGateway(namespace)
+  it('activates namespace in Portal', () => {
+    cy.executeCliCommand('gwa gateway current').then((response) => {
+      namespace = response.stdout.match(/\bgw-\w+/g)[0]
+      cy.activateGateway(namespace)
+    })
   })
 
   it('Verify that the product created through gwa command is displayed in the portal', () => {
     cy.visit(pd.path)
-    pd.editProductEnvironment('my-service API', 'dev')
+    pd.editProductEnvironment(serviceName + ' API', 'dev')
   })
 
   it('Verify the Authorization scope and issuer details for the product', () => {
@@ -77,11 +92,7 @@ describe('Verify CLI commands for generate/apply config', () => {
 
   it('Verify that the dataset created through GWA comand is assocuated with the product', () => {
     cy.visit(pd.path)
-    pd.verifyDataset('my-service', 'my-service API')
-  })
-
-  it('Navigate to home path', () => {
-    cy.visit(login.path)
+    pd.verifyDataset(serviceName, serviceName + ' API')
   })
 
   after(() => {
