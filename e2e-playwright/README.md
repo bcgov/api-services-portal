f21a6e9e56e6868d3a18f0614c03171781837c45
Russell Vinegar <russell.vinegar@gov.bc.ca>
2025-01-20 09:30:42 -0800

    Added 'used service' test to 07-endpoint

--- e2e-playwright/README.md
+++ e2e-playwright/README.md
@@ -6,6 +6,26 @@ npm install
 npx playwright install
 ```
 
+## Write tests
+
+### Cypress vs Playwright
+
+Key changes to convert tests from Cypress:
+
+1. Changed `describe`/`it` to `test.describe`/`test.it`
+1. Changed `before` to `test.beforeAll`
+1. Changed `cy.log` to `console.log`
+1. Changed promise chains to async/await
+1. Updated to use Playwright's `expect` assertions
+1. Slightly modified `callAPI` helper   
+1. Imported necessary functions from helpers (not required with Cypress commands)
+1. Set `test.describe.configure({ mode: 'serial' });` if tests in a file must be run in serial 
+   and `fullyParallel` is set to `true` in `playwright.config.ts`.
+
+### Codegen
+
+Auto generate tests with Codegen using `npx playwright codegen`.  
+
 ## Run tests
 
 npx playwright test
@@ -23,24 +43,14 @@ npx playwright test example
 npx playwright test --debug
   Runs the tests in debug mode.
 
-npx playwright codegen
-  Auto generate tests with Codegen.
+### Test reporters
 
-## Cypress vs Playwright
+There are a few reporter options, including `html`, the concise `list`, the even
+more concise `line` and `dot`. Multiple reporters can be used at once.
 
-Key changes to convert tests from Cypress:
+See [Reporters](https://playwright.dev/docs/test-reporters).
 
-1. Changed `describe`/`it` to `test.describe`/`test.it`
-1. Changed `before` to `test.beforeAll`
-1. Changed `cy.log` to `console.log`
-1. Changed promise chains to async/await
-1. Updated to use Playwright's `expect` assertions
-1. Slightly modified `callAPI` helper   
-1. Imported necessary functions from helpers (not required with Cypress commands)
-1. Set `test.describe.configure({ mode: 'serial' });` if tests in a file must be run in serial 
-   and `fullyParallel` is set to `true` in `playwright.config.ts`.
-
-## Debugging Playwright failures
+### Debugging test failures
 
 When a test fails, Playwright will print minimal details in the console, which
 are also included in the HTML report. These would be the line the test failed
@@ -55,19 +65,28 @@ For more info, see [Recording a
 Trace](https://playwright.dev/docs/trace-viewer-intro#recording-a-trace) in the
 Playwright docs.
 
-## Speeding up tests
+### Speeding up tests
 
-### Parallel tests
+#### Parallel tests
 
-In Playwright, tests are run in [parallel](https://playwright.dev/docs/test-parallel) by default. This is great for speeding
-up tests, but it can also cause issues if you have tests that depend on each
-other. So best to avoid that, at least within files.
+In Playwright, tests are run in
+[parallel](https://playwright.dev/docs/test-parallel) by default. This is great
+for speeding up tests, but it can also cause issues if you have tests that
+depend on each other. So best to avoid that, at least within files.
+
+To better handle dependencies, we should consider using:
+- global setup/teardown for the whole test run;
+- [Project dependencies](https://playwright.dev/docs/test-projects#dependencies)
+- [Worker-scoped fixtures](https://playwright.dev/docs/test-fixtures#worker-scoped-fixtures) for all tests run by a single worker
+- beforeAll/afterAll for all tests in a single file/describe.
+- if need be, [this workaround](https://github.com/microsoft/playwright/issues/22520#issuecomment-2391025061) for cross-worker communication
 
 To speed things up even more, one can not only run tests in parallel on a given
 machine (using multiple workers), but also on multiple machines at once.
 Playwright calls this [sharding](https://playwright.dev/docs/test-sharding).
+Sharding is the recommended approach for parallelization in CI (where )
 
-### Fail fast
+#### Fail fast
 
 Use `--only-changed` to run only tests that have changed since the last run
 first, then run all tests afterwards. See [Fail fast](https://playwright.dev/docs/ci#fail-fast)
--- e2e-playwright/tests/api-v3/07-endpoints.spec.ts
+++ e2e-playwright/tests/api-v3/07-endpoints.spec.ts
@@ -1,7 +1,12 @@
+import { exec } from 'child_process';
 import { test, expect } from '@playwright/test';
 import { callAPI } from '../../helpers/api-helpers';
 import * as fs from 'fs';
 import * as path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
+import { promisify } from 'util';
+const execAsync = promisify(exec);
 
 test.describe('Endpoints - unused service', () => {
   test('GET /routes/availability', async ({ request }) => {
@@ -31,46 +36,100 @@ test.describe('Endpoints - unused service', () => {
   });
 });
 
-// The below test is commented out because it requires more 
-// elaborate setup to run.
+const randomId = uuidv4().replace(/-/g, '').substring(0, 4);
 
-// test.describe('Endpoints - used service', () => {
-//   let userSession: string;
-//   let commonTestData: any;
+test.describe('Endpoints - used service', () => {
+  let authToken: string;
+  let commonTestData: any;
 
-//   test.beforeAll(async () => {
-//     // Load test data
-//     const testDataPath = path.join(__dirname, '../../fixtures/common-testdata.json');
-//     commonTestData = JSON.parse(fs.readFileSync(testDataPath, 'utf8'));
-//   });
+  test.beforeAll(async () => {
+    // Load test data
+    const testDataPath = path.join(__dirname, '../../fixtures/common-testdata.json');
+    commonTestData = JSON.parse(fs.readFileSync(testDataPath, 'utf8'));
+  });
 
-//   test('GET /routes/availability for used service', async ({ request }) => {
-//     const response = await callAPI(
-//       request,
-//       'routes/availability?gatewayId=gw-1234&serviceName=taken-service-name',
-//       'GET'
-//     );
+  test('Log in and get access token', async ({ page }) => {
+    await page.goto('/login?identity=provider&f=%2Fauth_callback%3Fidentity%3Dprovider');
+    await page.getByTestId('login-with-idir').click();
+    await page.getByLabel('Username').click();
+    await page.getByLabel('Username').fill('janis@idir');
+    await page.getByLabel('Username').press('Tab');
+    await page.getByLabel('Password').fill('awsummer');
+    await page.getByRole('button', { name: 'Sign In' }).click();
+    await expect(page.getByTestId('auth-menu-user')).toContainText('JS');
     
-//     const match = {
-//       available: false,
-//       suggestion: {
-//         serviceName: "gw-1234-taken-service-name",
-//         names: [
-//           "gw-1234-taken-service-name",
-//           "gw-1234-taken-service-name-dev",
-//           "gw-1234-taken-service-name-test"
-//         ],
-//         hosts: [
-//           "gw-1234-taken-service-name.api.gov.bc.ca",
-//           "gw-1234-taken-service-name.dev.api.gov.bc.ca",
-//           "gw-1234-taken-service-name.test.api.gov.bc.ca",
-//           "gw-1234-taken-service-name-api-gov-bc-ca.dev.api.gov.bc.ca",
-//           "gw-1234-taken-service-name-api-gov-bc-ca.test.api.gov.bc.ca"
-//         ]
-//       },
-//     };
+    const [response] = await Promise.all([
+      page.waitForResponse('**/admin/session'),
+      page.goto('/admin/session')
+    ]);
+    authToken = response.headers()['x-auth-request-access-token'];
+  });
+
+  test('Set CLI environment', async ({ baseURL}) => {
+    const cleanedUrl = baseURL.replace(/^http?:\/\//i, '');
+    const command = `gwa config set --host ${cleanedUrl} --scheme http`;
+    const { stdout } = await execAsync(command);
+    expect(stdout).toContain('Config settings saved');
+  });
+
+  test('Set CLI token', async () => {
+    const command = `gwa config set --token ${authToken}`;
+    const { stdout } = await execAsync(command);
+    expect(stdout).toContain('Config settings saved');
+  });
+
+  test('Create namespace with CLI', async () => {
+    const { serviceAvailability } = await commonTestData;
+    const command = `gwa gateway create --gateway-id ${serviceAvailability.namespace}-${randomId} --display-name="Service Availability"`;
+    const { stdout } = await execAsync(command);
+    expect(stdout).toContain(`${serviceAvailability.namespace}-${randomId}`);
+  });
+
+  test('Apply config for a service', async () => {
+    const { serviceAvailability } = await commonTestData;
+    // Read and modify the config file content
+    const configPath = path.join(__dirname, '../../fixtures/service-availability.yml');
+    let configContent = fs.readFileSync(configPath, 'utf8');
+    configContent = configContent.replace(/service-av/g, `${serviceAvailability.namespace}-${randomId}`);
+    configContent = configContent.replace(/taken-service-name/g, `taken-service-name-${randomId}`);
     
-//     expect(response.apiRes.status).toBe(200);
-//     expect(JSON.stringify(response.apiRes.body)).toBe(JSON.stringify(match));
-//   });
-// }); 
+    // Write to a temporary file
+    const tempConfigPath = path.join(__dirname, '../../fixtures/temp-service-availability.yml');
+    fs.writeFileSync(tempConfigPath, configContent);
+    
+    const command = `gwa apply -i ./fixtures/temp-service-availability.yml`;
+    const { stdout } = await execAsync(command);
+    expect(stdout).toContain('Gateway Services published');
+  });
+
+  test('GET /routes/availability for used service', async ({ request }) => {
+    await new Promise(resolve => setTimeout(resolve, 100));
+    const response = await callAPI(
+      request,
+      `routes/availability?gatewayId=gw-1234&serviceName=taken-service-name-${randomId}`,
+      'GET'
+    );
+    
+    const match = {
+      available: false,
+      suggestion: {
+        serviceName: `gw-1234-taken-service-name-${randomId}`,
+        names: [
+          `gw-1234-taken-service-name-${randomId}`,
+          `gw-1234-taken-service-name-${randomId}-dev`,
+          `gw-1234-taken-service-name-${randomId}-test`
+        ],
+        hosts: [
+          `gw-1234-taken-service-name-${randomId}.api.gov.bc.ca`,
+          `gw-1234-taken-service-name-${randomId}.dev.api.gov.bc.ca`,
+          `gw-1234-taken-service-name-${randomId}.test.api.gov.bc.ca`,
+          `gw-1234-taken-service-name-${randomId}-api-gov-bc-ca.dev.api.gov.bc.ca`,
+          `gw-1234-taken-service-name-${randomId}-api-gov-bc-ca.test.api.gov.bc.ca`
+        ]
+      },
+    };
+    
+    expect(response.apiRes.status).toBe(200);
+    expect(JSON.stringify(response.apiRes.body)).toBe(JSON.stringify(match));
+  });
+}); 
