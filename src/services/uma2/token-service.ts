a26fc3d1691bd38bc041c0c562f3ab2fa5ffe813
ikethecoder <ikethecoder@copeconsulting.ca>
2025-02-18 22:26:37 -0800

    throw error when error getting list of gateways

--- src/services/uma2/token-service.ts
+++ src/services/uma2/token-service.ts
@@ -97,7 +97,9 @@ export class UMA2TokenService {
           return res.json();
         } else if (res.status == 403) {
           // users that have no resources will get a 403, so gracefully handle that as "access to no resources"
-          return [];
+          return new Promise((resolve) => {
+            resolve([]);
+          });
         } else {
           return checkStatus(res);
         }
