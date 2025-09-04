5d68b2d29fd8e34925869ddb1bba5d303a811750
ikethecoder <ikethecoder@copeconsulting.ca>
2025-09-04 14:28:16 -0700

    add get api_spec

--- src/controllers/v3/OrgAPISpecController.ts
+++ src/controllers/v3/OrgAPISpecController.ts
@@ -6,7 +6,8 @@ import {
   Path,
   Route,
   Security,
-  Body, Tags
+  Body, Tags,
+  Get
 } from 'tsoa';
 import { KeystoneService } from '../ioc/keystoneInjector';
 import { inject, injectable } from 'tsyringe';
@@ -46,20 +47,39 @@ export class OrgAPISpecController extends Controller {
   ): Promise<{id: string}> {
     const ctx = await this.keystone.createContextithUser(request, true);
 
-    return await UpdateAPISpec(ctx, body.specUrl, body.productEnvAppId);
-  }  
-}
-
-const createAccessRequest = gql`
-  mutation OrgAccessRequestCreate ($data: OrgAccessRequestCreateInput) {
-    orgCreateAccessRequest (data: $data) {
-      application {
-        appId
-      }
-      accessRequest {
-        id
-      }
-    }
+    const result = await UpdateAPISpec(ctx, body.specUrl, body.productEnvAppId);
+    logger.debug('OrgAPISpecController: %j', result);
+    return {id: result.id}
   }
-`;
 
+  @Get('/{org}/api_specs')
+  @OperationId('organization-get-api-specs')
+  @Security('jwt', ['Namespace.Assign'])
+  public async get(
+    @Path() org: string,
+    @Request() request: any
+  ): Promise<string[]> {  
+    const ctx = await this.keystone.createContextithUser(request, true); 
+    const query = gql`
+      query getProductEnvironments($org: String) {
+        allEnvironments(where: { product: { organization: { name: $org } } }) {
+          id
+          spec {
+            id
+            blob
+          }
+        }
+          `;
+
+    const specs = await ctx.executeGraphQL({
+      query,
+      variables: { org },
+    });
+    if (specs.errors) {
+      logger.error('Error fetching Specs %j', specs.errors);
+      throw new Error('Error fetching Specs');
+    }
+    return specs.data.allEnvironments.filter((env:any) => env.spec).map((env: any) => env.spec);
+  }
+
+}
