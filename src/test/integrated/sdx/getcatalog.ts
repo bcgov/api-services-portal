ac8b478adf8e75a4360282f8096ded76ca865c85
ikethecoder <ikethecoder@copeconsulting.ca>
2025-12-03 22:04:35 -0800

    upd catalog

--- src/test/integrated/sdx/getcatalog.ts
+++ src/test/integrated/sdx/getcatalog.ts
@@ -49,10 +49,10 @@ import { GetConfigUsingPattern } from '../../../services/sdx/gateway-patterns';
   o(
     await GetConfigUsingPattern(ctx, {
       pattern: 'sdx-p2p-consumer-pub-r1',
-      locator: 'DEV.MIN.CITZ.SINGLE-DIGITAL-GW',
       parameters: {
-        provider: 'DEV.MIN.PUKI.TOYS',
         req_id: '12345',
+        consumer: 'DEV.MIN.CITZ.SINGLE-DIGITAL-GW',
+        provider: 'DEV.MIN.PUKI.TOYS',
       },
     })
   );
@@ -60,10 +60,10 @@ import { GetConfigUsingPattern } from '../../../services/sdx/gateway-patterns';
   o(
     await GetConfigUsingPattern(ctx, {
       pattern: 'sdx-p2p-provider-pub-r1',
-      locator: 'DEV.MIN.PUKI.TOYS',
       parameters: {
-        consumer: 'DEV.MIN.CITZ.SINGLE-DIGITAL-GW',
         req_id: '12345',
+        consumer: 'DEV.MIN.CITZ.SINGLE-DIGITAL-GW',
+        provider: 'DEV.MIN.PUKI.TOYS',
         upstream_uri: 'https://httpbun.com',
       },
     })
