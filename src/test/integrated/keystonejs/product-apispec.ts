fc56ef96844b8b349fd0a213edc798e2c18ff758
ikethecoder <ikethecoder@copeconsulting.ca>
2025-12-16 22:08:41 -0800

    integrated test for product apispec

--- src/test/integrated/keystonejs/product-apispec.ts
+++ src/test/integrated/keystonejs/product-apispec.ts
@@ -18,14 +18,35 @@ import {
   parseBlobString,
 } from '../../../batch/feed-worker';
 import { o } from '../util';
-import { lookupServiceAccessesByEnvironment } from '../../../services/keystone';
+import { dynamicallySetEnvironmentDetails, lookupServiceAccessesByEnvironment } from '../../../services/keystone';
 import {
   getActivity,
   recordActivity,
   recordActivityWithBlob,
 } from '../../../services/keystone/activity';
 import { id } from 'date-fns/locale';
-import {UpdateAPISpec, GetAPISpecsByOrg} from '../../../services/workflow/api-specs';
+import {
+  UpdateAPISpec,
+  GetAPISpecsByOrg,
+} from '../../../services/workflow/api-specs';
+import { Environment } from '../../../services/keystone/types';
+import { gql } from 'graphql-request';
+import YAML from 'yaml';
+import { OrgNamespace } from '../../../services/org-groups/types';
+import { getGwaProductEnvironment } from '../../../services/workflow';
+import { NamespaceService } from '../../../services/org-groups';
+
+async function getNamespaceAttributes(
+  ctx: any,
+  ns: string
+): Promise<OrgNamespace> {
+  const prodEnv = await getGwaProductEnvironment(ctx, false);
+  const envConfig = prodEnv.issuerEnvConfig;
+
+  const svc = new NamespaceService(envConfig.issuerUrl);
+  await svc.login(envConfig.clientId, envConfig.clientSecret);
+  return await svc.getNamespaceOrganizationDetails(ns);
+}
 
 (async () => {
   const keystone = await InitKeystone();
@@ -52,18 +73,94 @@ import {UpdateAPISpec, GetAPISpecsByOrg} from '../../../services/workflow/api-sp
   });
 
   if (true) {
-    const result = await GetAPISpecsByOrg(ctx, 'ministry-of-puppies-and-kittens');  
+    const list = gql`
+      query OrgProductCatalog {
+        allEnvironments {
+          appId
+          name
+          spec {
+            blob
+          }
+          credentialIssuer {
+            name
+            clientId
+            inheritFrom {
+              environmentDetails
+            } 
+          }
+          product {
+            name
+            namespace
+            organization {
+              name
+            }
+          }
+        }
+      }
+    `;
+
+    const result = await keystone.executeGraphQL({
+      context: ctx,
+      query: list,
+    });
+    o(result);
+    const envs = result.data.allEnvironments.filter(
+      (e: Environment) => e.product.organization != null
+    );
+
+    const output = envs.map((env: any) => {
+      if (env.credentialIssuer != null) {
+        const envDetails = JSON.parse(dynamicallySetEnvironmentDetails(env.credentialIssuer));
+        const credEnv = envDetails.find((e: any) => e.environment === env.name);
+        o(env)
+        env.credentialIssuer = {
+          issuerUrl: credEnv?.issuerUrl,
+          clientId: credEnv?.clientId,
+        }
+          o(env)
+      }
+    });
+
+  //     o(env.credentialIssuer?.environmentDetails)
+  //     return
+  //     const envs = JSON.parse(env.credentialIssuer?.environmentDetails);
+
+  //     const issuerEnv = envs?.find((e: any) => e.environment === env.name);
+  //     o(issuerEnv); 
+  //     return {
+  //       appId: env.appId,
+  //       name: env.name,
+  //       credentialIssuer: {
+  //         issuer: issuerEnv.issuerUrl,
+  //         clientId: issuerEnv.clientId,
+  //       },
+  //       product: {
+  //         name: env.product.name,
+  //         namespace: env.product.namespace,
+  //         organization: {
+  //           name: env.product.organization.name,
+  //         },
+  //       },
+  //     };
+  //   });
+  //   o(output);
+  }
+
+  if (false) {
+    const result = await GetAPISpecsByOrg(
+      ctx,
+      'ministry-of-puppies-and-kittens'
+    );
     o(result);
   }
   if (false) {
-    const spec=  'https://bcgov.github.io/sdx-openapi/%3CService%3E.v1.yaml';
-    const result = await UpdateAPISpec(ctx, spec, 'E7FEB796');  
+    const spec = 'https://bcgov.github.io/sdx-openapi/%3CService%3E.v1.yaml';
+    const result = await UpdateAPISpec(ctx, spec, 'E7FEB796');
     o(result);
   }
 
   if (false) {
     // upgrade
-
 
     const variables = {
       id: '20',
@@ -131,7 +228,7 @@ patterns:
              }`,
       variables,
     });
-    o(getSpec);    
+    o(getSpec);
     console.log(getSpec.data.allEnvironments[0].spec.blob);
   }
 
