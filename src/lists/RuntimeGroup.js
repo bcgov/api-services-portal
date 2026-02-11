f09ee53fa4ddb5b44689b0f7093bdc2409b63cf9
ikethecoder <ikethecoder@copeconsulting.ca>
2026-02-11 13:25:19 -0800

    fix runtime group name check

--- src/lists/RuntimeGroup.js
+++ src/lists/RuntimeGroup.js
@@ -47,7 +47,7 @@ module.exports = {
       }
       return resolvedData;
     },
-    validateInput: ({ resolvedData }) => {
+    validateInput: ({ operation, resolvedData }) => {
       if (operation == 'create') {
         new RuntimeGroupService().validateRuntimeGroup(resolvedData['name']);
       }
