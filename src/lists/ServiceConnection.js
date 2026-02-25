52fbb8bfb3eb051ce7806afea4d9d7b897edcdcc
ikethecoder <ikethecoder@copeconsulting.ca>
2026-02-24 22:56:49 -0800

    remove the autoincrement

--- src/lists/ServiceConnection.js
+++ src/lists/ServiceConnection.js
@@ -1,15 +1,8 @@
 const { Text } = require('@keystonejs/fields');
 const { EnforcementPoint } = require('../authz/enforcement');
-const { AutoIncrement } = require('@keystonejs/fields-auto-increment');
 
 module.exports = {
   fields: {
-    ref_id: {
-      type: AutoIncrement,
-      gqlType: 'Int',
-      isUnique: true,
-      access: { update: false },
-    },
     client_id: {
       type: Text,
       isRequired: true,
