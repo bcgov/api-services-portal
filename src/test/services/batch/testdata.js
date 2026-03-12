982c9bb01ae7994e2bbe1829c7da1d5e861a04be
James Elson <james.elson@gov.bc.ca>
2026-03-12 09:19:11 -0700

    Fix last remaining test

--- src/test/services/batch/testdata.js
+++ src/test/services/batch/testdata.js
@@ -81,7 +81,7 @@ export default {
         payload: {
           status: 400,
           result: 'update-failed',
-          reason: 'Failed updating children\n\n1 !== 0\n',
+          reason: 'Failed updating children',
           childResults: [
             {
               status: 400,
