e88fe127093c6656fd40b8c8bc8db21f86a8b95d
ikethecoder <ikethecoder@copeconsulting.ca>
2026-04-28 06:50:34 -0700

    fix remaining cypress test for sdx

--- e2e/cypress/tests/21-sdx-api/v1/02-oas-services.ts
+++ e2e/cypress/tests/21-sdx-api/v1/02-oas-services.ts
@@ -2,6 +2,7 @@ describe('SDX OpenAPI Services', () => {
   let workingData: any
 
   beforeEach(() => {
+    cy.fixture('toys.v1.yaml', null).as('toys.v1')
     cy.buildOrgGatewayDatasetAndProduct().then((data) => {
       workingData = data
     })
@@ -30,26 +31,23 @@ describe('SDX OpenAPI Services', () => {
         ({ apiRes: { status } }: any) => {
           expect(status).to.be.equal(200)
 
-          cy.request({
-            url: 'https://bcgov.github.io/sdx-openapi/Toys.v1.yaml',
-          }).then((fileResponse) => {
-            const blob = Cypress.Blob.binaryStringToBlob(
-              fileResponse.body,
-              'application/x-yaml'
-            )
-            blob.text().then((text) => {
-              cy.setRequestBody(text)
-              cy.setHeader('Content-Type', 'application/x-yaml')
-              cy.callAPI(
-                `ds/api/sdx/v1/organizations/${org.name}/oas-services?subsystem=${payload.name}`,
-                'PUT',
-                false
-              ).then(({ apiRes: { status, body } }: any) => {
-                expect(status).to.be.equal(200)
-                expect(body.result).to.be.equal('created')
-                expect(typeof body.id).to.be.equal('string')
-                expect(body).has.property('refKey')
-              })
+          cy.get('@toys.v1').then((text: any) => {
+            expect(Cypress.Buffer.isBuffer(text)).to.be.true
+            const body = text.toString()
+            expect(body).to.include('openapi: 3.1.1')
+
+            cy.setRequestBodyRaw(body)
+            cy.setHeader('Content-Type', 'application/octet-stream')
+
+            cy.callAPI(
+              `ds/api/sdx/v1/organizations/${org.name}/oas-services?subsystem=${payload.name}`,
+              'PUT',
+              false
+            ).then(({ apiRes: { status, body } }: any) => {
+              expect(status).to.be.equal(200)
+              expect(body.result).to.be.equal('created')
+              expect(typeof body.id).to.be.equal('string')
+              expect(body).has.property('refKey')
             })
           })
         }
