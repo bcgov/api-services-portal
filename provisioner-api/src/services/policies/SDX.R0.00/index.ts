d660365f4b97b2842f75cf8d4595f6c78e972b2a
ikethecoder <ikethecoder@copeconsulting.ca>
2026-06-24 21:49:03 -0700

    fix policy for connection

--- provisioner-api/src/services/policies/SDX.R0.00/index.ts
+++ provisioner-api/src/services/policies/SDX.R0.00/index.ts
@@ -23,7 +23,10 @@ const rawPolicies = ['connection.cedar'].map((file) => {
 const policies: Record<string, string> = {};
 rawPolicies.forEach((p) => {
   p.parts.forEach((part, index) => {
-    policies[`${p.name}:${index}`] = part;
+    // get the label from the first line as a comment after the permit|forbid keyword
+    const labelMatch = part.split('\n')[0].split('//')[1];
+    const label = labelMatch ? labelMatch.trim() : `${index}`;
+    policies[`${p.name}:${label}`] = part;
   });
 });
 
